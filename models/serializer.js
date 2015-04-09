a75657ff5ec9b9c074c570c7e5e8aed622833dfd
John F <john@neatstreet.net>
2015-04-08 22:23:33 -0400

    Remove unnecessary functions

--- models/serializer.js
+++ models/serializer.js
@@ -1,196 +1,196 @@
-var xmlBuilder    = require('xmlbuilder')
-  , dateFormatter = require('./date_formatter')
+var xmlBuilder      = require('xmlbuilder')
+    , dateFormatter = require('./date_formatter')
 
 /**
  * Creates the XML for an XML-RPC method call.
  *
- * @param {String} method     - The method name.
- * @param {Array} params      - Params to pass in the call.
+ * @param {String} method         - The method name.
+ * @param {Array} params            - Params to pass in the call.
  * @param {Function} callback - function (error, xml) { ... }
- *   - {Object|null} error    - Any errors that occurred while building the XML,
- *                              otherwise null.
- *   - {String} xml           - The method call XML.
+ *     - {Object|null} error        - Any errors that occurred while building the XML,
+ *                                                            otherwise null.
+ *     - {String} xml                     - The method call XML.
  */
 exports.serializeMethodCall = function(method, params) {
-  var params = params || [];
+    var params = params || [];
 
-  var xml = xmlBuilder
-    .create('methodCall')
-    .ele('methodName')
-      .txt(method)
-    .up()
-    .ele('params');
+    var xml = xmlBuilder
+        .create('methodCall')
+        .ele('methodName')
+            .txt(method)
+        .up()
+        .ele('params');
 
-  params.forEach(function(param) {
-    serializeValue(param, xml.ele('param'))
+    params.forEach(function(param) {
+        serializeValue(param, xml.ele('param'))
 });
 
-  // Includes the <?xml ...> declaration
+    // Includes the <?xml ...> declaration
 
-  var xmlString = xml.doc().toString()
-  return xmlString;
+    var xmlString = xml.doc().toString()
+    return xmlString;
 }
 
 /**
  * Creates the XML for an XML-RPC method response.
  *
- * @param {mixed} value       - The value to pass in the response.
+ * @param {mixed} value             - The value to pass in the response.
  * @param {Function} callback - function (error, xml) { ... }
- *   - {Object|null} error    - Any errors that occurred while building the XML,
- *                              otherwise null.
- *   - {String} xml           - The method response XML.
+ *     - {Object|null} error        - Any errors that occurred while building the XML,
+ *                                                            otherwise null.
+ *     - {String} xml                     - The method response XML.
  */
 exports.serializeMethodResponse = function(result) {
-  var xml = xmlBuilder.create()
-    . begin('methodResponse', { version: '1.0' })
-    . ele('params')
-      . ele('param')
+    var xml = xmlBuilder.create()
+        . begin('methodResponse', { version: '1.0' })
+        . ele('params')
+            . ele('param')
 
-  serializeValue(result, xml)
+    serializeValue(result, xml)
 
-  // Includes the <?xml ...> declaration
-  return xml.doc().toString()
+    // Includes the <?xml ...> declaration
+    return xml.doc().toString()
 }
 
 exports.serializeFault = function(fault) {
-  var xml = xmlBuilder.create()
-    . begin('methodResponse', { version: '1.0' })
-    . ele('fault')
+    var xml = xmlBuilder.create()
+        . begin('methodResponse', { version: '1.0' })
+        . ele('fault')
 
-  serializeValue(fault, xml)
+    serializeValue(fault, xml)
 
-  // Includes the <?xml ...> declaration
-  return xml.doc().toString()
+    // Includes the <?xml ...> declaration
+    return xml.doc().toString()
 }
 
 function serializeValue(value, xml) {
-  var stack     = [ { value: value, xml: xml } ]
-    , current   = null
-    , valueNode = null
-    , next      = null
+    var stack         = [ { value: value, xml: xml } ]
+        , current     = null
+        , valueNode = null
+        , next            = null
 
-  while (stack.length > 0) {
-    current = stack[stack.length - 1]
+    while (stack.length > 0) {
+        current = stack[stack.length - 1]
 
-    if (current.index !== undefined) {
-      // Iterating a compound
-      next = getNextItemsFrame(current)
-      if (next) {
-        stack.push(next)
-      }
-      else {
-        stack.pop()
-      }
-    }
-    else {
-      // we're about to add a new value (compound or simple)
-      valueNode = current.xml.ele('value')
-      switch(typeof current.value) {
-        case 'boolean':
-          appendBoolean(current.value, valueNode)
-          stack.pop()
-          break
-        case 'string':
-          appendString(current.value, valueNode)
-          stack.pop()
-          break
-        case 'number':
-          appendNumber(current.value, valueNode)
-          stack.pop()
-          break
-        case 'object':
-          if (current.value === null) {
-            valueNode.ele('nil')
-            stack.pop()
-          }
-          else if (current.value instanceof Date) {
-            appendDatetime(current.value, valueNode)
-            stack.pop()
-          }
-          else if (Buffer.isBuffer(current.value)) {
-            appendBuffer(current.value, valueNode)
-            stack.pop()
-          }
-          else {
-            if (Array.isArray(current.value)) {
-              current.xml = valueNode.ele('array').ele('data')
-            }
-            else {
-              current.xml = valueNode.ele('struct')
-              current.keys = Object.keys(current.value)
-            }
-            current.index = 0
+        if (current.index !== undefined) {
+            // Iterating a compound
             next = getNextItemsFrame(current)
             if (next) {
-              stack.push(next)
+                stack.push(next)
             }
             else {
-              stack.pop()
+                stack.pop()
             }
-          }
-          break
-        default:
-          stack.pop()
-          break
-      }
+        }
+        else {
+            // we're about to add a new value (compound or simple)
+            valueNode = current.xml.ele('value')
+            switch(typeof current.value) {
+                case 'boolean':
+                    appendBoolean(current.value, valueNode)
+                    stack.pop()
+                    break
+                case 'string':
+                    appendString(current.value, valueNode)
+                    stack.pop()
+                    break
+                case 'number':
+                    appendNumber(current.value, valueNode)
+                    stack.pop()
+                    break
+                case 'object':
+                    if (current.value === null) {
+                        valueNode.ele('nil')
+                        stack.pop()
+                    }
+                    else if (current.value instanceof Date) {
+                        appendDatetime(current.value, valueNode)
+                        stack.pop()
+                    }
+                    else if (Buffer.isBuffer(current.value)) {
+                        appendBuffer(current.value, valueNode)
+                        stack.pop()
+                    }
+                    else {
+                        if (Array.isArray(current.value)) {
+                            current.xml = valueNode.ele('array').ele('data')
+                        }
+                        else {
+                            current.xml = valueNode.ele('struct')
+                            current.keys = Object.keys(current.value)
+                        }
+                        current.index = 0
+                        next = getNextItemsFrame(current)
+                        if (next) {
+                            stack.push(next)
+                        }
+                        else {
+                            stack.pop()
+                        }
+                    }
+                    break
+                default:
+                    stack.pop()
+                    break
+            }
+        }
     }
-  }
 }
 
 function getNextItemsFrame(frame) {
-  var nextFrame = null
+    var nextFrame = null
 
-  if (frame.keys) {
-    if (frame.index < frame.keys.length) {
-      var key    = frame.keys[frame.index++]
-        , member = frame.xml.ele('member').ele('name').text(key).up()
-      nextFrame = {
-        value: frame.value[key]
-      , xml: member
-      }
+    if (frame.keys) {
+        if (frame.index < frame.keys.length) {
+            var key        = frame.keys[frame.index++]
+                , member = frame.xml.ele('member').ele('name').text(key).up()
+            nextFrame = {
+                value: frame.value[key]
+            , xml: member
+            }
+        }
     }
-  }
-  else if (frame.index < frame.value.length) {
-    nextFrame = {
-      value: frame.value[frame.index]
-    , xml: frame.xml
+    else if (frame.index < frame.value.length) {
+        nextFrame = {
+            value: frame.value[frame.index]
+        , xml: frame.xml
+        }
+        frame.index++
     }
-    frame.index++
-  }
 
-  return nextFrame
+    return nextFrame
 }
 
 function appendBoolean(value, xml) {
-  xml.ele('boolean').txt(value ? 1 : 0)
+    xml.ele('boolean').txt(value ? 1 : 0)
 }
 
 var illegalChars = /^(?![^<&]*]]>[^<&]*)[^<&]*$/
 function appendString(value, xml) {
-  if (value.length === 0) {
-    xml.ele('string')
-  }
-  else if (!illegalChars.test(value)) {
-    xml.ele('string').d(value)
-  }
-  else {
-    xml.ele('string').txt(value)
-  }
+    if (value.length === 0) {
+        xml.ele('string')
+    }
+    else if (!illegalChars.test(value)) {
+        xml.ele('string').d(value)
+    }
+    else {
+        xml.ele('string').txt(value)
+    }
 }
 
 function appendNumber(value, xml) {
-  if (value % 1 == 0) {
-    xml.ele('int').txt(value)
-  }
-  else {
-    xml.ele('double').txt(value)
-  }
+    if (value % 1 == 0) {
+        xml.ele('int').txt(value)
+    }
+    else {
+        xml.ele('double').txt(value)
+    }
 }
 
 function appendDatetime(value, xml) {
-  xml.ele('dateTime.iso8601').txt(dateFormatter.encodeIso8601(value))
+    xml.ele('dateTime.iso8601').txt(dateFormatter.encodeIso8601(value))
 }
 
 function appendBuffer(value, xml) {
-  xml.ele('base64').txt(value.toString('base64'))
+    xml.ele('base64').txt(value.toString('base64'))
 }
