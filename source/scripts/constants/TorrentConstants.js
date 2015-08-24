6fd06a60d91f76999852c007849259d7739b1680
John Furrow <johnf@mesosphere.io>
2015-08-23 17:34:28 -0700

    Add failure constants for actions

--- source/scripts/constants/TorrentConstants.js
+++ source/scripts/constants/TorrentConstants.js
@@ -1,10 +1,13 @@
 var keyMirror = require('keymirror');
 
 module.exports = keyMirror({
-  TORRENT_ADD_URL: 'torrent--add--url',
+  TORRENT_ADD_SUCCESS: 'torrent__add--success',
+  TORRENT_ADD_FAIL: 'torrent__add--fail',
   TORRENT_CLICK: 'torrent--click',
-  TORRENT_STOP: 'torrent--stop',
-  TORRENT_START: 'torrent--start',
+  TORRENT_STOP_SUCCESS: 'torrent--stop',
+  TORRENT_STOP_FAIL: 'torrent--stop',
+  TORRENT_START_SUCCESS: 'torrent__start--success',
+  TORRENT_START_FAIL: 'torrent__start--fail',
   TORRENT_LIST_CHANGE: 'torrent-list--change',
   TORRENT_SELECTION_CHANGE: 'torrent--select--change'
 });
