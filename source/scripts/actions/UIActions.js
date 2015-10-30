1c22319a1b2249388b2c374779429d33d6731b75
John Furrow <johnf@mesosphere.io>
2015-10-29 20:32:44 -0500

    Add actions for searching & filtering torrents

--- source/scripts/actions/UIActions.js
+++ source/scripts/actions/UIActions.js
@@ -5,6 +5,20 @@ export function handleTorrentClick(payload) {
   };
 }
 
+export function setTorrentsFilter(payload) {
+  return {
+    type: 'UI_FILTER_TORRENTS',
+    payload
+  };
+}
+
+export function setTorrentsSearch(payload) {
+  return {
+    type: 'UI_SEARCH_TORRENTS',
+    payload
+  };
+}
+
 export function setTorrentsSort(payload) {
   return {
     type: 'UI_SORT_TORRENTS',
