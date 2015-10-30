c07369583a5ec32f75b8b22192979a5936d049ef
John Furrow <johnf@mesosphere.io>
2015-10-30 00:11:26 -0700

    Remove debug messages

--- source/scripts/util/searchTorrents.js
+++ source/scripts/util/searchTorrents.js
@@ -5,13 +5,9 @@ export function searchTorrents(torrentList, torrentListSearchString) {
     let queries = [];
     let searchTerms = torrentListSearchString.replace(/,/g, ' ').split(' ');
 
-    console.log(searchTerms);
-
     for (let i = 0, len = searchTerms.length; i < len; i++) {
       queries.push(new RegExp(searchTerms[i], 'gi'));
     }
-
-    console.log(queries);
 
     searchedTorrents = searchedTorrents.filter(torrent => {
       for (let i = 0, len = queries.length; i < len; i++) {
