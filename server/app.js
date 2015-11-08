ed52c8ecb5ad61cde574b210e9a035ed361001f9
John Furrow <johnf@mesosphere.io>
2015-11-08 00:46:39 -0800

    Change directory of static assets

--- server/app.js
+++ server/app.js
@@ -12,13 +12,11 @@ var client = require('./routes/client');
 var app = express();
 
 // view engine setup
-console.log(__dirname);
-
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(__dirname + '/public/favicon.ico'));
+// uncomment after placing your favicon in /assets
+//app.use(favicon(__dirname + '/assets/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
