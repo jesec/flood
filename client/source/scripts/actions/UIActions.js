d7e002179d0f7b36cfbae27e7733501a5782378e
John Furrow <johnf@mesosphere.io>
2015-11-08 00:49:47 -0800

    Add generic modals actions

--- client/source/scripts/actions/UIActions.js
+++ client/source/scripts/actions/UIActions.js
@@ -1,3 +1,19 @@
+export function displayModal(payload) {
+  return {
+    type: 'UI_DISPLAY_MODAL',
+    payload
+  };
+}
+
+export function dismissModal(payload) {
+  return {
+    type: 'UI_DISPLAY_MODAL',
+    payload: {
+      modal: null
+    }
+  };
+}
+
 export function handleTorrentClick(payload) {
   return {
     type: 'CLICK_TORRENT',
