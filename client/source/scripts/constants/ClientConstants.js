7260661d9b7689bcc3a7d0a1117d018730dca55e
John Furrow <johnf@mesosphere.io>
2015-12-14 21:29:20 -0800

    Add constants

--- client/source/scripts/constants/ClientConstants.js
+++
@@ -1,9 +0,0 @@
-import keyMirror from 'keymirror';
-
-const ClientConstants = keyMirror({
-  ADD_TORRENT: 'client--add-torrent',
-  REMOVE_TORRENT: 'client--remove-torrent',
-  CLIENT_STATS_CHANGE: 'client--stats-change'
-});
-
-export default ClientConstants;
