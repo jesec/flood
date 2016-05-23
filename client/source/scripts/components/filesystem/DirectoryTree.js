15130809f8065a5da922c7b71c306f8f918df961
John Furrow <johnf@mesosphere.io>
2016-05-22 21:09:57 -0700

    Sort files at end of directory tree

--- client/source/scripts/components/filesystem/DirectoryTree.js
+++ client/source/scripts/components/filesystem/DirectoryTree.js
@@ -19,7 +19,19 @@ export default class DirectoryTree extends React.Component {
     let hash = this.props.hash;
     depth++;
 
-    return Object.keys(tree).map((branchName) => {
+    let directories = Object.keys(tree).sort((a, b) => {
+      if (a === 'files') {
+        return 1;
+      }
+
+      if (b === 'files') {
+        return -1;
+      }
+
+      return a.localeCompare(b);
+    });
+
+    return directories.map((branchName) => {
       let branch = tree[branchName];
       index++;
 
