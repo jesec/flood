4de782ecf1c5f2fb9cc4abcf8528fe7c5d9cb561
Matthijs Hoekstra <matthijshoekstra@me.com>
2016-06-19 22:58:00 +0200

    Add Dutch translations

--- client/scripts/i18n/nl.js
+++ client/scripts/i18n/nl.js
@@ -1,3 +1,15 @@
 export default {
+  "filter.all": "Alles",
+
+  "statusfilter.title": "Filter op Status",
+  "statusfilter.all": "Alles",
+  "statusfilter.downloading": "Downloaden",
+  "statusfilter.completed": "Klaar",
+  "statusfilter.active": "Actief",
+  "statusfilter.inactive": "Inactief",
+  "statusfilter.error": "Fout",
+
+  "searchbox.placeholder": "Zoek torrents",
+
   "sidebar.settings_button": "Instellingen"
 };
