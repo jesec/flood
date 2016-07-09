280413bf7ebe7e55d906552aa6a282164d190ca6
John Furrow <johnf@mesosphere.io>
2016-07-09 13:07:40 -0700

    Use user-defined config based on supplied template

---
+++ config.template.js
@@ -0,0 +1,16 @@
+const config = {
+  dbCleanInterval: 1000 * 60 * 60,
+  dbPath: './server/db/',
+  floodServerPort: 3000,
+  maxHistoryStates: 30,
+  pollInterval: 1000 * 5,
+  secret: 'flood',
+  scgi: {
+    host: 'localhost',
+    port: 5000,
+    socket: false,
+    socketPath: '/tmp/rtorrent.sock'
+  }
+};
+
+module.exports = config;
