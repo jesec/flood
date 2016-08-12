60eb7b0715a6be187184330b233c6e38f6d1fd19
Stéphane de Wit <contact@stephanedewit.be>
2016-08-12 22:37:11 +0200

    Fix torrents with more than one tracker
    'getCalculatedTrackers' method returned only the first domain.
    
    Global regEx matching should not be used on a shared regEx instance.

--- shared/util/regEx.js
+++ shared/util/regEx.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const regEx = {
-  domainName: /https?:\/\/(?:www\.)?([-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,18}\b)*(\/[\/\d\w\.-]*)*(?:[\?])*(.+)*/gi
+  domainName: /https?:\/\/(?:www\.)?([-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,18}\b)*(\/[\/\d\w\.-]*)*(?:[\?])*(.+)*/i
 };
 
 module.exports = regEx;
