9471d8bf0d376898e32498751df5e0d69526c247
Saldru <alex-du-71@orange.fr>
2016-08-27 11:20:51 +0200

    Adding French Language

--- client/scripts/i18n/index.js
+++ client/scripts/i18n/index.js
@@ -1,12 +1,15 @@
 import {addLocaleData, IntlProvider} from 'react-intl';
 import deLocaleData from 'react-intl/locale-data/de';
 import enLocaleData from 'react-intl/locale-data/en';
+import frLocaleData from 'react-intl/locale-data/fr';
 import nlLocaleData from 'react-intl/locale-data/nl';
 
 addLocaleData(deLocaleData);
 addLocaleData(enLocaleData);
+addLocaleData(frLocaleData);
 addLocaleData(nlLocaleData);
 
 export {default as de} from './de';
 export {default as en} from './en';
+export {default as fr} from './fr';
 export {default as nl} from './nl';
