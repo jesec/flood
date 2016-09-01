168b349d38131087e681e0ca4a5f0a3bd7dfd2af
John Furrow <johnf@mesosphere.io>
2016-08-31 21:48:36 -0700

    Process file data on server

--- server/util/clientResponseUtil.js
+++ server/util/clientResponseUtil.js
@@ -3,8 +3,35 @@
 let clientUtil = require('./clientUtil');
 let util = require('util');
 
+let getFileTreeFromPathsArr = (tree, directory, file, depth) => {
+  if (depth == null) {
+    depth = 0;
+  }
+
+  if (tree == null) {
+    tree = {};
+  }
+
+  if (depth++ < file.pathComponents.length - 1) {
+    if (!tree.directories) {
+      tree.directories = {};
+    }
+
+    tree.directories[directory] = getFileTreeFromPathsArr(
+      tree.directories[directory], file.pathComponents[depth], file, depth);
+  } else {
+    if (!tree.files) {
+      tree.files = [];
+    }
+
+    tree.files.push(clientResponseUtil.processFile(file));
+  }
+
+  return tree;
+};
+
 let clientResponseUtil = {
-  processTorrentDetails: (data) => {
+  processTorrentDetails(data) {
     // TODO: This is ugly.
     let peersData = data[0][0] || null;
     let filesData = data[1][0] || null;
@@ -12,6 +39,7 @@ let clientResponseUtil = {
     let peers = null;
     let files = null;
     let trackers = null;
+    let fileTree = {};
 
     if (peersData && peersData.length) {
       peers = clientUtil.mapClientProps(
@@ -26,13 +54,9 @@ let clientResponseUtil = {
         filesData
       );
 
-      files = files.map((file) => {
-        file.filename = file.pathComponents[file.pathComponents.length - 1];
-        file.percentComplete = (file.completedChunks / file.sizeChunks * 100).toFixed(0);
-        delete(file.completedChunks);
-        delete(file.sizeChunks);
-        return file;
-      });
+      fileTree = files.reduce((memo, file) => {
+        return getFileTreeFromPathsArr(memo, file.pathComponents[0], file);
+      }, {});
     }
 
     if (trackerData && trackerData.length) {
@@ -42,10 +66,21 @@ let clientResponseUtil = {
       );
     }
 
-    return {peers, files, trackers};
+    return {peers, trackers, fileTree};
   },
 
-  processTransferStats: (data) => {
+  processFile(file) {
+    file.filename = file.pathComponents[file.pathComponents.length - 1];
+    file.percentComplete = (file.completedChunks / file.sizeChunks * 100).toFixed(0);
+
+    delete file.completedChunks;
+    delete file.pathComponents;
+    delete file.sizeChunks;
+
+    return file;
+  },
+
+  processTransferStats(data) {
     return clientUtil.mapClientProps(clientUtil.defaults.clientProperties,
       data);
   }
