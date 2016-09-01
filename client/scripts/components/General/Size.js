4d7722d75ccbe00f5f669d42d1a9efe51a5f9835
Stéphane de Wit <contact@stephanedewit.be>
2016-09-01 23:26:30 +0200

    Create a component to display sizes

---
+++ client/scripts/components/General/Size.js
@@ -0,0 +1,74 @@
+import React from 'react';
+
+export default class Size extends React.Component {
+  compute(bytes, extraUnits, precision, padded) {
+    let kilobyte = 1024,
+      megabyte = kilobyte * 1024,
+      gigabyte = megabyte * 1024,
+      terabyte = gigabyte * 1024,
+      value = 0,
+      unit = '';
+
+    if ((bytes >= 0) && (bytes < kilobyte)) {
+      value = bytes;
+      unit = 'B';
+    } else if ((bytes >= kilobyte) && (bytes < megabyte)) {
+      value = (bytes / kilobyte);
+      unit = 'KB';
+    } else if ((bytes >= megabyte) && (bytes < gigabyte)) {
+      value = (bytes / megabyte);
+      unit = 'MB';
+    } else if ((bytes >= gigabyte) && (bytes < terabyte)) {
+      value = (bytes / gigabyte);
+      unit = 'GB';
+    } else if (bytes >= terabyte) {
+      value = (bytes / terabyte);
+      unit = 'TB';
+    } else {
+      value = bytes;
+      unit = 'B';
+    }
+
+    value = Number(value);
+    if (!!value && value < 10) {
+      value = Number(value.toFixed(precision));
+    } else if (!!value && value > 10 && value < 100) {
+      value = Number(value.toFixed(precision - 1));
+    } else if (!!value && value > 100) {
+      value = Math.floor(value);
+    }
+
+    if (padded === false) {
+      let decimal = value % 1;
+      if ((decimal < 0.1 && decimal >= 0) || (decimal > -0.1 && decimal <= 0)) {
+        value = value.toFixed(0);
+      }
+    }
+
+    if (extraUnits) {
+      unit += extraUnits;
+    }
+
+    return {
+      value,
+      unit
+    };
+  }
+
+  render() {
+    let {value, unit} = this.compute(this.props.value, this.props.extraUnits, this.props.precision, this.props.padded);
+
+    return (
+      <span>
+        {value}
+        <em className="unit">{unit}</em>
+      </span>
+    );
+  }
+}
+
+Size.defaultProps = {
+  extraUnits: '',
+  precision: 2,
+  padded: true
+};
