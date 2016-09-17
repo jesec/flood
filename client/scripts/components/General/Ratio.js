05a40d386048e7802d221c1a1607d3b8385750f6
Stéphane de Wit <contact@stephanedewit.be>
2016-09-17 17:42:16 +0200

    Create a Ratio component

---
+++ client/scripts/components/General/Ratio.js
@@ -0,0 +1,23 @@
+import {FormattedNumber} from 'react-intl';
+import React from 'react';
+
+export default class Ratio extends React.Component {
+  render() {
+    let ratio = this.props.value;
+
+    ratio = ratio / 1000;
+    let precision = 1;
+
+    if (ratio < 10) {
+      precision = 2;
+    } else if (ratio < 100) {
+      precision = 0;
+    }
+
+    ratio = ratio.toFixed(precision);
+
+    return (
+      <FormattedNumber value={ratio} />
+    );
+  }
+}
