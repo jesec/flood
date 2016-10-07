823915e2bb0bc5db0954290079b2fbea35cc2b81
John Furrow <johnf@mesosphere.io>
2016-10-06 20:12:42 -0700

    Remove errored torrents

--- server/models/TorrentNotificationService.js
+++ server/models/TorrentNotificationService.js
@@ -4,15 +4,6 @@ let NotificationCollection = require('./NotificationCollection');
 let propsMap = require('../../shared/constants/propsMap');
 
 class NotificationService {
-  hasError(data) {
-    return data.status && data.status.indexOf(propsMap.clientStatus.error) > -1;
-  }
-
-  hasErrored(prevData, nextData) {
-    return prevData && prevData.status && !this.hasError(prevData)
-      && this.hasError(nextData);
-  }
-
   hasFinished(prevData, nextData) {
     return prevData.percentComplete < 100 && nextData.percentComplete === 100;
   }
@@ -22,15 +13,6 @@ class NotificationService {
       NotificationCollection.addNotification({
         id: 'notification.torrent.finished',
         data: {name: nextData.name}
-      });
-    }
-
-    if (this.hasErrored(prevData, nextData)) {
-      let error = nextData.message ? ` (${nextData.message})` : '';
-
-      NotificationCollection.addNotification({
-        id: 'notification.torrent.errored',
-        data: {name: nextData.name, error}
       });
     }
   }
