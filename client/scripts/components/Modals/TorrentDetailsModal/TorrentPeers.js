01957e83963a4031c03b1f1b4da651620bf4c99b
John Furrow <johnf@mesosphere.io>
2016-11-16 22:48:04 -0800

    Add country code & flag to peers table

--- client/scripts/components/Modals/TorrentDetailsModal/TorrentPeers.js
+++ client/scripts/components/Modals/TorrentDetailsModal/TorrentPeers.js
@@ -8,14 +8,58 @@ import FolderOpenSolid from '../../Icons/FolderOpenSolid';
 import Size from '../../General/Size';
 
 export default class TorrentPeers extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {
+      erroredCountryImages: []
+    };
+  }
+
+  getImageErrorHandlerFn(countryCode) {
+    return () => {
+      let {erroredCountryImages} = this.state;
+      erroredCountryImages.push(countryCode);
+      this.setState({erroredCountryImages});
+    };
+  }
+
   render() {
-    let peers = this.props.peers;
+    let {peers} = this.props;
 
     if (peers) {
+      let {erroredCountryImages} = this.state;
       let peerList = peers.map((peer, index) => {
+        let countryCode = peer.country;
+        let peerCountry = null;
+
+        if (countryCode) {
+          let image = null;
+
+          if (!erroredCountryImages.includes(countryCode)) {
+            image = (
+              <img className="peers-list__flag__image"
+                onError={this.getImageErrorHandlerFn(countryCode)}
+                src={`images/flags/${countryCode.toLowerCase()}.png`} />
+            );
+          }
+
+          peerCountry = (
+            <span className="peers-list__flag">
+              {image}
+              <span className="peers-list__flag__text">
+                {countryCode}
+              </span>
+            </span>
+          );
+        }
+
         return (
           <tr key={index}>
-            <td>{peer.address}</td>
+            <td>
+              {peerCountry}
+              {peer.address}
+            </td>
             <td>
               <Size value={peer.downloadRate} isSpeed={true} />
             </td>
@@ -27,7 +71,7 @@ export default class TorrentPeers extends React.Component {
       });
 
       return (
-        <div className="torrent-details__peers torrent-details__section">
+        <div className="torrent-details__section torrent-details__section--peers">
           <table className="torrent-details__table table">
             <thead className="torrent-details__table__heading">
               <tr>
