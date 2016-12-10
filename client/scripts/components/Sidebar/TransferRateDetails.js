9576621c7d6e414d79bfa3a69d84455d8e795da1
John Furrow <johnf@mesosphere.io>
2016-12-09 21:55:45 -0800

    Display infinity for unlimited speed

--- client/scripts/components/Sidebar/TransferRateDetails.js
+++ client/scripts/components/Sidebar/TransferRateDetails.js
@@ -1,4 +1,5 @@
 import classnames from 'classnames';
+import {FormattedMessage} from 'react-intl';
 import React from 'react';
 
 import Download from '../Icons/Download';
@@ -62,6 +63,17 @@ class TransferRateDetails extends React.Component {
       );
     }
 
+    let limit = null;
+
+    console.log(throttles[slug]);
+    if (throttles[slug] === 0) {
+      console.log('zero')
+      limit = <FormattedMessage id="unit.time.infinity" defaultMessage="∞" />;
+    } else {
+      console.log('not zero')
+      limit = <Size value={throttles[slug]} isSpeed={true} />;
+    }
+
     return (
       <div className={`client-stats__rate client-stats__rate--${slug}`}>
         <div className="client-stats__rate__icon">
@@ -77,7 +89,7 @@ class TransferRateDetails extends React.Component {
               <Size value={transferTotals[slug]} />
             </div>
             <div className="client-stats__rate__data--limit">
-              <Size value={throttles[slug]} isSpeed={true} />
+              {limit}
             </div>
           </div>
         </div>
