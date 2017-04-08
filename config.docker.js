96f1f1c75b0d5373f6124c74e98b28f14d1a183a
Bryan Alves <bryanalves@gmail.com>
2017-04-07 23:31:51 -0400

    Add support for Docker
    Including a configuration file that allows setting of most settings by
    injecting environment variables into the container.

---
+++ config.docker.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+  baseURI: process.env.FLOOD_BASE_URI || '/',
+  dbCleanInterval: 1000 * 60 * 60,
+  dbPath: '/data/server/db/',
+  floodServerPort: 3000,
+  maxHistoryStates: 30,
+  pollInterval: 1000 * 5,
+  secret: process.env.FLOOD_SECRET || 'flood',
+  scgi: {
+    host: process.env.RTORRENT_SCGI_HOST || 'localhost',
+    port: process.env.RTORRENT_SCGI_PORT || 5000,
+    socket: process.env.RTORRENT_SOCK == 'true',
+    socketPath: '/data/rtorrent.sock'
+  },
+  ssl: process.env.FLOOD_ENABLE_SSL == 'true',
+  sslKey: '/data/flood_ssl.key',
+  sslCert: '/data/flood_ssl.cert'
+};
+
+module.exports = CONFIG;
