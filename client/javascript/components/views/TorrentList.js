72b0ba4aebc944d917551b9112b26485ac55e801
John Furrow <johnf@mesosphere.io>
2017-04-23 10:49:09 -0700

    Eliminate 'panel' components

--- client/javascript/components/views/TorrentList.js
+++ client/javascript/components/views/TorrentList.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import ActionBar from '../torrent-list/ActionBar';
 import Alerts from '../alerts/Alerts';
-import ApplicationView from '../layout/ApplicationView';
 import ApplicationContent from '../layout/ApplicationContent';
+import ApplicationPanel from '../layout/ApplicationPanel';
+import ApplicationView from '../layout/ApplicationView';
 import Modals from '../modals/Modals';
-import Sidebar from '../panels/Sidebar';
 import SettingsStore from '../../stores/SettingsStore';
+import Sidebar from '../sidebar/Sidebar';
 import TorrentActions from '../../actions/TorrentActions';
-import TorrentList from '../panels/TorrentList';
+import TorrentList from '../torrent-list/TorrentList';
 
 export default class TorrentListView extends React.Component {
   componentDidMount() {
@@ -21,7 +23,10 @@ export default class TorrentListView extends React.Component {
       <ApplicationView>
         <Sidebar />
         <ApplicationContent>
-          <TorrentList />
+          <ApplicationPanel modifier="torrent-list" className="view--torrent-list">
+            <ActionBar />
+            <TorrentList />
+          </ApplicationPanel>
           <Modals />
           <Alerts />
         </ApplicationContent>
