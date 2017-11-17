ff9efd2d4346aacc7f0449d05d706dd2268b898d
John Furrow <hello@johnfurrow.com>
2017-11-16 20:06:37 -0800

    Clean up code and styles

--- client/src/javascript/components/sidebar/LogoutButton.js
+++ client/src/javascript/components/sidebar/LogoutButton.js
@@ -1,10 +1,18 @@
+import {defineMessages, injectIntl} from 'react-intl';
 import React from 'react';
 
 import AuthActions from '../../actions/AuthActions';
 import Logout from '../icons/Logout';
 import Tooltip from '../general/Tooltip';
 
-class NotificationsButton extends React.Component {
+const MESSAGES = defineMessages({
+  logOut: {
+    id: 'sidebar.button.log.out',
+    defaultMessage: 'Log Out'
+  }
+});
+
+class LogoutButton extends React.Component {
   handleLogoutClick() {
     AuthActions.logout().then(() => {
       window.location.reload();
@@ -14,16 +22,16 @@ class NotificationsButton extends React.Component {
   render() {
     return (
       <Tooltip
-        content="Logout"
+        content={this.props.intl.formatMessage(MESSAGES.logOut)}
+        onClick={this.handleLogoutClick}
         position="bottom"
-        wrapperClassName="sidebar__action sidebar__icon-button
-          sidebar__icon-button--interactive tooltip__wrapper">
-        <div onClick={this.handleLogoutClick}>
-          <Logout />
-        </div>
+        wrapperClassName="sidebar__action sidebar__action--last
+          sidebar__icon-button sidebar__icon-button--interactive
+          tooltip__wrapper">
+        <Logout />
       </Tooltip>
     );
   }
 }
 
-export default NotificationsButton;
+export default injectIntl(LogoutButton);
