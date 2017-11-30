8447ffa5b770e72f778fdece1bc9344b17c39d69
John Furrow <hello@johnfurrow.com>
2017-11-29 19:56:42 -0800

    Ensure req.body is defined

--- server/middleware/booleanCoerce.js
+++ server/middleware/booleanCoerce.js
@@ -2,7 +2,7 @@
 
 module.exports = key => {
   return (req, res, next) => {
-    const value = req.body[key];
+    const value = req.body && req.body[key];
 
     if (value && typeof value === 'string') {
       req.body[key] = value === 'true';
