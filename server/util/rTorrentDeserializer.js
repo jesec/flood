jesec/flood

commit 47b2053ac3a6ce78ba57f9396b3487be69877b63

rTorrentDeserializer: unescape XML special chars
Bug: Flood-UI/flood#843, jesec/flood#13
Cherry-Pick: Flood-UI/flood#842
Signed-off-by: Jesse Chan <jc@linux.com>

server/util/rTorrentDeserializer.js
@@ -7,6 +7,14 @@ let dataIsVal;
 let endOfResponse;
 let rejectCallback;
 
+const unescapeXMLString = (value) =>
+  value
+    .replace(/&amp;/g, '&')
+    .replace(/&lt;/g, '<')
+    .replace(/&gt;/g, '>')
+    .replace(/&apos;/g, "'")
+    .replace(/&quot;/g, '"');
+
 const openTag = (elementName) => {
   if (elementName === 'array' || elementName === 'struct') {
     stackMarks.push(dataStack.length);
@@ -16,7 +24,7 @@ const openTag = (elementName) => {
 };
 
 const onText = (value) => {
-  tmpData.push(value);
+  tmpData.push(unescapeXMLString(value));
 };
 
 const onError = (err) => {

commit 63775d4648d2a2b3d4bac31934b8a4d337fffadb

rTorrentDeserializer: avoid double unescaping
```
Escape characters themselves (such as ampersand in the case of HTML
encoding) play a special role during escaping and unescaping: they
are themselves escaped, but also form part of the escaped represent
-ations of other characters. Hence care must be taken to avoid double
escaping and unescaping: when escaping, the escape character must be
escaped first, when unescaping it has to be unescaped last.
```

Fixes: 47b2053a

server/util/rTorrentDeserializer.js
@@ -9,11 +9,11 @@ let rejectCallback;
 
 const unescapeXMLString = (value) =>
   value
-    .replace(/&amp;/g, '&')
     .replace(/&lt;/g, '<')
     .replace(/&gt;/g, '>')
     .replace(/&apos;/g, "'")
-    .replace(/&quot;/g, '"');
+    .replace(/&quot;/g, '"')
+    .replace(/&amp;/g, '&');
 
 const openTag = (elementName) => {
   if (elementName === 'array' || elementName === 'struct') {