f23f31a6f2d41bf619566b0b239aa8053b48298e
Jesse Chan <jc@linux.com>
2020-10-11 11:32:24 +0800

    server: migrations: add migration for UserInDatabase2

---
+++ server/bin/migrations/UserInDatabase2.ts
@@ -0,0 +1,96 @@
+import {AccessLevel} from '../../../shared/schema/Auth';
+import Users from '../../models/Users';
+
+import type {Credentials} from '../../../shared/schema/Auth';
+import type {RTorrentConnectionSettings} from '../../../shared/schema/ClientConnectionSettings';
+import type {UserInDatabase1} from './types/UserInDatabase1';
+
+const migrationError = (err?: Error) => {
+  if (err) {
+    console.error(err);
+  }
+  console.error('Migration failed. You need to reset the databases manually.');
+  process.exit();
+};
+
+const migration = () => {
+  return new Promise((resolve, _reject) => {
+    Users.listUsers((users, err) => {
+      if (users == null || err) {
+        return;
+      }
+
+      Promise.all(
+        users.map((user) => {
+          return new Promise((migratedResolve, _migratedReject) => {
+            if (user.client != null) {
+              // No need to migrate.
+              migratedResolve();
+              return;
+            }
+
+            const userV1 = (user as unknown) as UserInDatabase1;
+
+            let connectionSettings: RTorrentConnectionSettings | null = null;
+            if (userV1.socketPath != null) {
+              connectionSettings = {
+                client: 'rTorrent',
+                type: 'socket',
+                version: 1,
+                socket: userV1.socketPath,
+              };
+            } else if (userV1.host != null && userV1.port != null) {
+              connectionSettings = {
+                client: 'rTorrent',
+                type: 'tcp',
+                version: 1,
+                host: userV1.host,
+                port: userV1.port,
+              };
+            }
+
+            if (connectionSettings == null) {
+              migrationError(new Error('Corrupted client connection settings.'));
+              return;
+            }
+
+            const userV2: Credentials = {
+              username: userV1.username,
+              password: userV1.password,
+              client: connectionSettings,
+              level: userV1.isAdmin ? AccessLevel.ADMINISTRATOR : AccessLevel.USER,
+            };
+
+            Users.removeUser(userV1.username, (id, errRemoval) => {
+              if (errRemoval) {
+                migrationError(errRemoval);
+                return;
+              }
+
+              if (id == null) {
+                migrationError(new Error('Wrong user ID'));
+                return;
+              }
+
+              Users.createUser(
+                userV2,
+                (_username, errCreation) => {
+                  if (errCreation) {
+                    migrationError(errCreation);
+                  }
+
+                  migratedResolve();
+                },
+                false,
+              );
+            });
+          });
+        }),
+      ).then(() => {
+        resolve();
+      });
+    });
+  });
+};
+
+export default migration;
