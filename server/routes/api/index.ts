jesec/flood

commit dbde60b35ec38e6e0a734e981ca19e289baa1aa2

server: migrate getDirectoryList to TypeScript

server/routes/api/index.ts
@@ -1,8 +1,6 @@
 import express from 'express';
 import passport from 'passport';
 
-import type {Request} from 'express';
-
 import type {HistorySnapshot} from '@shared/constants/historySnapshotTypes';
 import type {NotificationFetchOptions} from '@shared/types/Notification';
 
@@ -12,7 +10,7 @@ import clientRoutes from './client';
 import clientActivityStream from '../../middleware/clientActivityStream';
 import eventStream from '../../middleware/eventStream';
 import feedMonitorRoutes from './feed-monitor';
-import Filesystem from '../../models/Filesystem';
+import {getDirectoryList} from '../../util/fileUtil';
 import settings from '../../models/settings';
 import torrentsRoutes from './torrents';
 
@@ -28,15 +26,22 @@ router.use('/torrents', torrentsRoutes);
 
 router.get('/activity-stream', eventStream, clientActivityStream);
 
-router.get('/directory-list', (req, res) => {
-  Filesystem.getDirectoryList(req.query, ajaxUtil.getResponseFn(res));
+router.get<unknown, unknown, unknown, string>('/directory-list', (req, res) => {
+  const callback = ajaxUtil.getResponseFn(res);
+  getDirectoryList(req.query)
+    .then((data) => {
+      callback(data);
+    })
+    .catch((error) => {
+      callback(null, error);
+    });
 });
 
-router.get('/history', (req: Request<unknown, unknown, unknown, {snapshot: HistorySnapshot}>, res) => {
+router.get<unknown, unknown, unknown, {snapshot: HistorySnapshot}>('/history', (req, res) => {
   req.services?.historyService.getHistory(req.query, ajaxUtil.getResponseFn(res));
 });
 
-router.get('/notifications', (req: Request<unknown, unknown, unknown, NotificationFetchOptions>, res) => {
+router.get<unknown, unknown, unknown, NotificationFetchOptions>('/notifications', (req, res) => {
   req.services?.notificationService.getNotifications(req.query, ajaxUtil.getResponseFn(res));
 });
 

server/models/Filesystem.js
@@ -1,45 +1,0 @@
-import fs from 'fs';
-import os from 'os';
-import path from 'path';
-
-import fileUtil from '../util/fileUtil';
-
-const getDirectoryList = (options, callback) => {
-  const sourcePath = (options.path || '/').replace(/^~/, os.homedir());
-
-  const resolvedPath = fileUtil.sanitizePath(sourcePath);
-  if (!fileUtil.isAllowedPath(resolvedPath)) {
-    callback(null, fileUtil.accessDeniedError());
-    return;
-  }
-
-  try {
-    const directories = [];
-    const files = [];
-
-    fs.readdirSync(resolvedPath).forEach((item) => {
-      const joinedPath = path.join(resolvedPath, item);
-      if (fs.existsSync(joinedPath)) {
-        if (fs.statSync(joinedPath).isDirectory()) {
-          directories.push(item);
-        } else {
-          files.push(item);
-        }
-      }
-    });
-
-    const hasParent = /^.{0,}:?(\/|\\){1,1}\S{1,}/.test(resolvedPath);
-
-    callback({
-      directories,
-      files,
-      hasParent,
-      path: resolvedPath,
-      separator: path.sep,
-    });
-  } catch (error) {
-    callback(null, error);
-  }
-};
-
-export default {getDirectoryList};

server/models/client.js
@@ -4,10 +4,10 @@ import sanitize from 'sanitize-filename';
 import {series} from 'async';
 import tar from 'tar-stream';
 
+import {accessDeniedError, createDirectory, isAllowedPath, sanitizePath} from '../util/fileUtil';
 import ClientRequest from './ClientRequest';
 import clientResponseUtil from '../util/clientResponseUtil';
 import {clientSettingsBiMap} from '../../shared/constants/clientSettingsMap';
-import fileUtil from '../util/fileUtil';
 import settings from './settings';
 import torrentFilePropsMap from '../../shared/constants/torrentFilePropsMap';
 import torrentPeerPropsMap from '../../shared/constants/torrentPeerPropsMap';
@@ -18,13 +18,13 @@ const client = {
   addFiles(user, services, options, callback) {
     const {destination: destinationPath, files, isBasePath, start, tags} = options;
 
-    const resolvedPath = fileUtil.sanitizePath(destinationPath);
-    if (!fileUtil.isAllowedPath(resolvedPath)) {
-      callback(null, fileUtil.accessDeniedError());
+    const resolvedPath = sanitizePath(destinationPath);
+    if (!isAllowedPath(resolvedPath)) {
+      callback(null, accessDeniedError());
       return;
     }
 
-    fileUtil.createDirectory({path: resolvedPath});
+    createDirectory({path: resolvedPath});
 
     // Each torrent is sent individually because rTorrent accepts a total
     // filesize of 524 kilobytes or less. This allows the user to send many
@@ -56,12 +56,12 @@ const client = {
   addUrls(user, services, data, callback) {
     const {urls, destination, isBasePath, start, tags} = data;
     const request = new ClientRequest(user, services);
-    const resolvedPath = fileUtil.sanitizePath(destination);
-    if (!fileUtil.isAllowedPath(resolvedPath)) {
-      callback(null, fileUtil.accessDeniedError());
+    const resolvedPath = sanitizePath(destination);
+    if (!isAllowedPath(resolvedPath)) {
+      callback(null, accessDeniedError());
       return;
     }
-    fileUtil.createDirectory({path: resolvedPath});
+    createDirectory({path: resolvedPath});
     request.addURLs({
       urls,
       path: resolvedPath,

server/services/clientGatewayService.ts
@@ -14,9 +14,9 @@ import type {
   StopTorrentsOptions,
 } from '@shared/types/Action';
 
+import {accessDeniedError, isAllowedPath, sanitizePath} from '../util/fileUtil';
 import BaseService from './BaseService';
 import fileListMethodCallConfigs from '../constants/fileListMethodCallConfigs';
-import fileUtil from '../util/fileUtil';
 import scgiUtil from '../util/scgiUtil';
 
 import type {MethodCallConfigs, MultiMethodCalls} from '../constants/rTorrentMethodCall';
@@ -100,9 +100,9 @@ class ClientGatewayService extends BaseService<ClientGatewayServiceEvents> {
    * @return {Promise} - Resolves with the processed client response or rejects with the processed client error.
    */
   async moveTorrents({hashes, destination, moveFiles, isBasePath, isCheckHash}: MoveTorrentsOptions) {
-    const resolvedPath = fileUtil.sanitizePath(destination);
-    if (!fileUtil.isAllowedPath(resolvedPath)) {
-      return Promise.reject(fileUtil.accessDeniedError());
+    const resolvedPath = sanitizePath(destination);
+    if (!isAllowedPath(resolvedPath)) {
+      throw accessDeniedError();
     }
 
     const hashesToRestart: Array<string> = [];
@@ -140,7 +140,7 @@ class ClientGatewayService extends BaseService<ClientGatewayServiceEvents> {
           return;
         }
 
-        const destinationFilePath = fileUtil.sanitizePath(path.join(resolvedPath, baseFileName));
+        const destinationFilePath = sanitizePath(path.join(resolvedPath, baseFileName));
         if (sourceBasePath !== destinationFilePath) {
           try {
             moveSync(sourceBasePath, destinationFilePath, {overwrite: true});

server/util/fileUtil.ts
@@ -1,19 +1,10 @@
 import fs from 'fs';
+import {homedir} from 'os';
 import path from 'path';
 
 import config from '../../config';
 
-const createDirectory = (options: {path: string}) => {
-  if (options.path) {
-    fs.mkdir(options.path, {recursive: true}, (error) => {
-      if (error) {
-        console.trace('Error creating directory.', error);
-      }
-    });
-  }
-};
-
-const isAllowedPath = (resolvedPath: string) => {
+export const isAllowedPath = (resolvedPath: string) => {
   if (config.allowedPaths == null) {
     return true;
   }
@@ -25,23 +16,57 @@ const isAllowedPath = (resolvedPath: string) => {
   });
 };
 
-const sanitizePath = (input: string) => {
+export const sanitizePath = (input: string) => {
   // eslint-disable-next-line no-control-regex
   const controlRe = /[\x00-\x1f\x80-\x9f]/g;
   return path.resolve(input).replace(controlRe, '');
 };
 
-const accessDeniedError = () => {
+export const accessDeniedError = () => {
   const error = new Error() as NodeJS.ErrnoException;
   error.code = 'EACCES';
   return error;
 };
 
-const fileUtil = {
-  createDirectory,
-  isAllowedPath,
-  sanitizePath,
-  accessDeniedError,
+export const createDirectory = (options: {path: string}) => {
+  if (options.path) {
+    fs.mkdir(options.path, {recursive: true}, (error) => {
+      if (error) {
+        console.trace('Error creating directory.', error);
+      }
+    });
+  }
 };
 
-export default fileUtil;
+export const getDirectoryList = async (inputPath: string) => {
+  const sourcePath = (inputPath || '/').replace(/^~/, homedir());
+
+  const resolvedPath = sanitizePath(sourcePath);
+  if (!isAllowedPath(resolvedPath)) {
+    throw accessDeniedError();
+  }
+
+  const directories: Array<string> = [];
+  const files: Array<string> = [];
+
+  fs.readdirSync(resolvedPath).forEach((item) => {
+    const joinedPath = path.join(resolvedPath, item);
+    if (fs.existsSync(joinedPath)) {
+      if (fs.statSync(joinedPath).isDirectory()) {
+        directories.push(item);
+      } else {
+        files.push(item);
+      }
+    }
+  });
+
+  const hasParent = /^.{0,}:?(\/|\\){1,1}\S{1,}/.test(resolvedPath);
+
+  return {
+    directories,
+    files,
+    hasParent,
+    path: resolvedPath,
+    separator: path.sep,
+  };
+};

commit a25ed1742268b6bbfea41a39c54ae33e9c34fcc0

server: api: fix /directory-list API mismatch
Fixes: dbde60b35ec38e6e0a734e981ca19e289baa1aa2

server/routes/api/index.ts
@@ -30,9 +30,9 @@ router.use('/torrents', torrentsRoutes);
 
 router.get('/activity-stream', eventStream, clientActivityStream);
 
-router.get<unknown, unknown, unknown, string>('/directory-list', (req, res) => {
+router.get<unknown, unknown, unknown, {path: string}>('/directory-list', (req, res) => {
   const callback = ajaxUtil.getResponseFn(res);
-  getDirectoryList(req.query)
+  getDirectoryList(req.query.path)
     .then((data) => {
       callback(data);
     })