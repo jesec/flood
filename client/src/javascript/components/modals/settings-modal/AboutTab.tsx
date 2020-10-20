a93c4f6ebf64890cd40251be771774a87999183f
Jesse Chan <jc@linux.com>
2020-10-20 17:49:00 +0800

    client: AboutTab: display Flood version badges

--- client/src/javascript/components/modals/settings-modal/AboutTab.tsx
+++ client/src/javascript/components/modals/settings-modal/AboutTab.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
 
+import {version} from '../../../../../../package.json';
+
 const AboutMarkdown = React.lazy(() =>
   import('../../../../../../ABOUT.md').then((module) => ({default: module.react})),
 );
 
+const FLOOD_PROJECT_URL = 'https://github.com/jesec/flood';
+
 const AboutTab = () => {
   return (
     <React.Suspense fallback={null}>
-      <AboutMarkdown />
+      <AboutMarkdown
+        FloodVersion={() => version}
+        CommitBadge={() =>
+          version.length > 8 ? (
+            // If user is on a rolling build, display latest version of rolling build.
+            <a href={FLOOD_PROJECT_URL}>
+              <img alt="Latest version of rolling build" src="https://img.shields.io/npm/v/@jesec/flood?label=HEAD" />
+            </a>
+          ) : (
+            // If user is on a released build, display commits to project made since user's version.
+            <a href={FLOOD_PROJECT_URL}>
+              <img
+                alt="Commits since user's version"
+                src={`https://img.shields.io/github/commits-since/jesec/flood/v${version}`}
+              />
+            </a>
+          )
+        }
+      />
     </React.Suspense>
   );
 };
