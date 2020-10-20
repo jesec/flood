jesec/flood

commit dcfa590f9f84a15b004881d5bd71fb67fe9ba3d7

feature: allow to attach cookies to fetch requests

server/util/tempFileUtil.ts
@@ -37,7 +37,7 @@ export const saveBufferToTempFile = async (buffer: Buffer, extension?: string): 
  * @param {string} extension - file extension of temp file
  * @return {string} - path of saved temporary file. deleted after 5 minutes.
  */
-export const fetchURLToTempFile = async (url: string, extension?: string): Promise<string> => {
+export const fetchURLToTempFile = async (url: string, cookies?: Array<string>, extension?: string): Promise<string> => {
   const tempPath = getTempFilePath(extension);
 
   await new Promise((resolve) => {
@@ -45,6 +45,9 @@ export const fetchURLToTempFile = async (url: string, extension?: string): Promi
       method: 'GET',
       url,
       responseType: 'stream',
+      headers: {
+        Cookie: cookies?.join('; ').concat(';'),
+      },
     }).then((res: AxiosResponse) => {
       res.data.pipe(fs.createWriteStream(tempPath)).on('finish', () => resolve());
     });

client/src/javascript/components/modals/add-torrents-modal/AddTorrentsByURL.tsx
@@ -12,6 +12,8 @@ import TorrentActions from '../../../actions/TorrentActions';
 type AddTorrentsByURLFormData = {
   [urls: string]: string;
 } & {
+  [cookies: string]: string;
+} & {
   destination: string;
   isBasePath: boolean;
   start: boolean;
@@ -51,8 +53,20 @@ class AddTorrentsByURL extends React.Component<AddTorrentsByURLProps, AddTorrent
       return;
     }
 
+    const urls = getTextArray(formData, 'urls');
+    const cookies = getTextArray(formData, 'cookies');
+
+    // TODO: handle multiple domain names
+    const firstDomain = urls[0].startsWith('http') && urls[0].split('/')[2];
+    const processedCookies = firstDomain
+      ? {
+          [firstDomain]: cookies,
+        }
+      : undefined;
+
     TorrentActions.addTorrentsByUrls({
-      urls: getTextArray(formData, 'urls'),
+      urls,
+      cookies: processedCookies,
       destination: formData.destination,
       isBasePath: formData.isBasePath || false,
       start: formData.start || false,
@@ -78,6 +92,15 @@ class AddTorrentsByURL extends React.Component<AddTorrentsByURLProps, AddTorrent
             id: 'torrents.add.tab.url.input.placeholder',
           })}
           defaultValues={this.state.urlTextboxes}
+        />
+        <TextboxRepeater
+          id="cookies"
+          label={this.props.intl.formatMessage({
+            id: 'torrents.add.cookies.label',
+          })}
+          placeholder={this.props.intl.formatMessage({
+            id: 'torrents.add.cookies.input.placeholder',
+          })}
         />
         <FilesystemBrowserTextbox
           id="destination"

server/services/rTorrent/clientGatewayService.ts
@@ -65,7 +65,7 @@ class RTorrentClientGatewayService extends ClientGatewayService {
     return this.addTorrentsByURL({urls: torrentPaths, destination, tags, isBasePath, start});
   }
 
-  async addTorrentsByURL({urls, destination, tags, isBasePath, start}: AddTorrentByURLOptions): Promise<void> {
+  async addTorrentsByURL({urls, cookies, destination, tags, isBasePath, start}: AddTorrentByURLOptions): Promise<void> {
     const destinationPath = sanitizePath(destination);
 
     if (!isAllowedPath(destinationPath)) {
@@ -77,8 +77,14 @@ class RTorrentClientGatewayService extends ClientGatewayService {
     const torrentPaths = await Promise.all(
       urls.map(async (url) => {
         if (/^(http|https):\/\//.test(url)) {
+          const domain = url.split('/')[2];
+
           // TODO: properly handle error and let frontend know
-          const torrentPath = await fetchURLToTempFile(url, 'torrent').catch((e) => console.error(e));
+          const torrentPath = await fetchURLToTempFile(
+            url,
+            cookies ? cookies[domain] : undefined,
+            'torrent',
+          ).catch((e) => console.error(e));
 
           if (typeof torrentPath === 'string') {
             return torrentPath;

shared/types/api/torrents.ts
@@ -5,6 +5,11 @@ import type {TorrentContentPriority} from '../TorrentContent';
 export interface AddTorrentByURLOptions {
   // URLs to download torrents from
   urls: Array<string>;
+  // Cookies to attach to requests
+  cookies?: {
+    // An array of strings in the format "name=value";
+    [domain: string]: Array<string>;
+  };
   // Path of destination
   destination: string;
   // Tags

commit 8178fc9260a1c726d58ba2ed5d84c13ddfe8391e

server: tempFileUtil: fix optional logics of cookies
Fixes: dcfa590f9f84a15b004881d5bd71fb67fe9ba3d7

server/util/tempFileUtil.ts
@@ -45,9 +45,11 @@ export const fetchURLToTempFile = async (url: string, cookies?: Array<string>, e
       method: 'GET',
       url,
       responseType: 'stream',
-      headers: {
-        Cookie: cookies?.join('; ').concat(';'),
-      },
+      headers: cookies
+        ? {
+            Cookie: cookies.join('; ').concat(';'),
+          }
+        : undefined,
     }).then((res: AxiosResponse) => {
       res.data.pipe(fs.createWriteStream(tempPath)).on('finish', () => resolve());
     });