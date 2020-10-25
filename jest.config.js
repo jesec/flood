8854c265d68a34eea7e274200a40fbec28631a10
Hyeronimous Zawapete <hyeronimous.zawapete@gmail.com>
2020-10-25 15:17:45 +0100

    HTTP_BASIC_AUTH_HANDLER

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,9 @@ module.exports = {
   verbose: true,
   collectCoverage: true,
   coverageProvider: 'v8',
-  projects: ['<rootDir>/server/.jest/auth.config.js', '<rootDir>/server/.jest/test.config.js'],
+  projects: [
+    '<rootDir>/server/.jest/auth.config.js',
+    '<rootDir>/server/.jest/httpauth.config.js',
+    '<rootDir>/server/.jest/test.config.js',
+  ],
 };
--- server/routes/api/auth.ts
+++ server/routes/api/auth.ts
@@ -316,7 +316,7 @@ router.use('/', passport.authenticate('jwt', {session: false}));
 router.get('/logout', (_req, res) => {
   res.clearCookie('jwt');
   if (config.enableUsersHTTPBasicAuthHandler) {
-    res.status(401).send('Unauthorized');
+    res.status(401).json('Unauthorized').send();
     return;
   }
 
