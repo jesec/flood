067057cb2f1d3d20f5c792bd04493bd350de8712
Jesse Chan <jc@linux.com>
2020-10-27 13:02:19 +0800

    WindowTitle: fix speed update and remove from translation

--- client/src/javascript/components/general/WindowTitle.tsx
+++ client/src/javascript/components/general/WindowTitle.tsx
@@ -9,47 +9,36 @@ const WindowTitle: React.FC = () => {
   const {transferSummary: summary} = TransferDataStore;
   const intl = useIntl();
 
-  React.useEffect(() => {
-    let title = 'Flood';
+  let title = 'Flood';
 
-    if (summary != null && Object.keys(summary).length > 0) {
-      const down = compute(summary.downRate);
-      const up = compute(summary.upRate);
+  if (summary != null && Object.keys(summary).length > 0) {
+    const down = compute(summary.downRate);
+    const up = compute(summary.upRate);
 
-      const formattedDownSpeed = intl.formatNumber(down.value);
-      const formattedUpSpeed = intl.formatNumber(up.value);
+    const formattedDownSpeed = intl.formatNumber(down.value);
+    const formattedUpSpeed = intl.formatNumber(up.value);
 
-      const translatedDownUnit = intl.formatMessage(
-        {
-          id: 'unit.speed',
-        },
-        {
-          baseUnit: intl.formatMessage({id: getTranslationString(down.unit)}),
-        },
-      );
-      const translatedUpUnit = intl.formatMessage(
-        {
-          id: 'unit.speed',
-        },
-        {
-          baseUnit: intl.formatMessage({id: getTranslationString(up.unit)}),
-        },
-      );
+    const translatedDownUnit = intl.formatMessage(
+      {
+        id: 'unit.speed',
+      },
+      {
+        baseUnit: intl.formatMessage({id: getTranslationString(down.unit)}),
+      },
+    );
+    const translatedUpUnit = intl.formatMessage(
+      {
+        id: 'unit.speed',
+      },
+      {
+        baseUnit: intl.formatMessage({id: getTranslationString(up.unit)}),
+      },
+    );
 
-      title = intl.formatMessage(
-        {
-          id: 'window.title',
-          // \u2193 and \u2191 are down and up arrows, respectively
-        },
-        {
-          down: `${formattedDownSpeed} ${translatedDownUnit}`,
-          up: `${formattedUpSpeed} ${translatedUpUnit}`,
-        },
-      );
-    }
+    title = `↓ ${formattedDownSpeed} ${translatedDownUnit} ↑ ${formattedUpSpeed} ${translatedUpUnit} - Flood`;
+  }
 
-    document.title = title;
-  }, [intl, summary]);
+  document.title = title;
 
   return null;
 };
