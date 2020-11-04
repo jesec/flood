39deb6f9a534eee5d2cd038a5cd1842dbd352b10
Jesse Chan <jc@linux.com>
2020-11-05 02:16:25 +0800

    FeedsTab: handle items with same titles

--- client/src/javascript/components/modals/feeds-modal/FeedsTab.tsx
+++ client/src/javascript/components/modals/feeds-modal/FeedsTab.tsx
@@ -346,6 +346,29 @@ class FeedsTab extends React.Component<WrappedComponentProps, FeedsTabStates> {
   getFeedItemsForm() {
     const {feeds, items} = FeedStore;
 
+    const itemElements: React.ReactNodeArray = [];
+    if (this.state.selectedFeedID) {
+      const titleOccurrences: Record<string, number> = {};
+      items.forEach((item, index) => {
+        let {title} = item;
+        const occurrence = titleOccurrences[title];
+
+        if (occurrence == null) {
+          titleOccurrences[title] = 2;
+        } else {
+          title = `${title} #${occurrence}`;
+          titleOccurrences[title] += 1;
+        }
+
+        itemElements.push(
+          <li className="interactive-list__item interactive-list__item--stacked-content feed-list__feed" key={title}>
+            <div className="interactive-list__label feed-list__feed-label">{title}</div>
+            <Checkbox id={`${index}`} />
+          </li>,
+        );
+      });
+    }
+
     return (
       <Form
         className="inverse"
@@ -398,9 +421,9 @@ class FeedsTab extends React.Component<WrappedComponentProps, FeedsTabStates> {
           {this.renderSearchField()}
           {this.renderDownloadButton()}
         </FormRow>
-        {this.state.selectedFeedID && (
+        {this.state.selectedFeedID ? (
           <FormRow>
-            {items.length === 0 ? (
+            {itemElements.length === 0 ? (
               <ul className="interactive-list">
                 <li className="interactive-list__item">
                   <div className="interactive-list__label">
@@ -409,19 +432,10 @@ class FeedsTab extends React.Component<WrappedComponentProps, FeedsTabStates> {
                 </li>
               </ul>
             ) : (
-              <ul className="interactive-list feed-list">
-                {items.map((item, index) => (
-                  <li
-                    className="interactive-list__item interactive-list__item--stacked-content feed-list__feed"
-                    key={item.title}>
-                    <div className="interactive-list__label feed-list__feed-label">{item.title}</div>
-                    <Checkbox id={`${index}`} />
-                  </li>
-                ))}
-              </ul>
+              <ul className="interactive-list feed-list">{itemElements}</ul>
             )}
           </FormRow>
-        )}
+        ) : null}
       </Form>
     );
   }
