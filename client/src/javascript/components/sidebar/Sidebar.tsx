03cad54eb6d6ce716264ad0f4c0f20fac741213b
Jesse Chan <jc@linux.com>
2020-12-02 23:28:42 +0800

    Sidebar: make theme button always at the bottom

--- client/src/javascript/components/sidebar/Sidebar.tsx
+++ client/src/javascript/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, ReactNode} from 'react';
 import {OverlayScrollbarsComponent} from 'overlayscrollbars-react';
 
 import DiskUsage from './DiskUsage';
@@ -14,6 +14,15 @@ import TagFilters from './TagFilters';
 import ThemeSwitchButton from './ThemeSwitchButton';
 import TrackerFilters from './TrackerFilters';
 import TransferData from './TransferData';
+
+const BottomContainer: FC<{children: ReactNode}> = ({children}: {children: ReactNode}) => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', flexGrow: 1}}>
+      <div style={{flexGrow: 1}} />
+      {children}
+    </div>
+  );
+};
 
 const Sidebar: FC = () => {
   return (
@@ -39,9 +48,11 @@ const Sidebar: FC = () => {
       <TagFilters />
       <TrackerFilters />
       <DiskUsage />
-      <SidebarActions>
-        <ThemeSwitchButton />
-      </SidebarActions>
+      <BottomContainer>
+        <SidebarActions>
+          <ThemeSwitchButton />
+        </SidebarActions>
+      </BottomContainer>
     </OverlayScrollbarsComponent>
   );
 };
