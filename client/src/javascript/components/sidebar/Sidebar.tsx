jesec/flood

commit 03cad54eb6d6ce716264ad0f4c0f20fac741213b

Sidebar: make theme button always at the bottom

client/src/javascript/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, ReactNode} from 'react';
 import {OverlayScrollbarsComponent} from 'overlayscrollbars-react';
 
 import DiskUsage from './DiskUsage';
@@ -14,6 +14,15 @@ import TagFilters from './TagFilters';
 import ThemeSwitchButton from './ThemeSwitchButton';
 import TrackerFilters from './TrackerFilters';
 import TransferData from './TransferData';
+
+const BottomContainer: FC<{children: ReactNode}> = ({children}: {children: ReactNode}) => {
+  return (
+    <div style={{display: 'flex', flexDirection: 'column', flexGrow: 1}}>
+      <div style={{flexGrow: 1}} />
+      {children}
+    </div>
+  );
+};
 
 const Sidebar: FC = () => {
   return (
@@ -39,9 +48,11 @@ const Sidebar: FC = () => {
       <TagFilters />
       <TrackerFilters />
       <DiskUsage />
-      <SidebarActions>
-        <ThemeSwitchButton />
-      </SidebarActions>
+      <BottomContainer>
+        <SidebarActions>
+          <ThemeSwitchButton />
+        </SidebarActions>
+      </BottomContainer>
     </OverlayScrollbarsComponent>
   );
 };

client/src/sass/components/_sidebar.scss
@@ -13,6 +13,11 @@
     z-index: 2;
     transition: transform 0.2s;
 
+    .os-content {
+      display: flex;
+      flex-direction: column;
+    }
+
     @media (max-width: 720px) {
       transform: translateX(-120px);
     }

commit 54bb9b998a6d85c9b67bdc4100928df8f35a30fe

Sidebar: simplify flexbox grow layouts
This change fixed sidebar style issues on Safari.

Fixes: 03cad54e

client/src/javascript/components/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import {FC, ReactNode} from 'react';
+import {FC} from 'react';
 import {OverlayScrollbarsComponent} from 'overlayscrollbars-react';
 
 import DiskUsage from './DiskUsage';
@@ -14,15 +14,6 @@ import TagFilters from './TagFilters';
 import ThemeSwitchButton from './ThemeSwitchButton';
 import TrackerFilters from './TrackerFilters';
 import TransferData from './TransferData';
-
-const BottomContainer: FC<{children: ReactNode}> = ({children}: {children: ReactNode}) => {
-  return (
-    <div style={{display: 'flex', flexDirection: 'column', flexGrow: 1}}>
-      <div style={{flexGrow: 1}} />
-      {children}
-    </div>
-  );
-};
 
 const Sidebar: FC = () => {
   return (
@@ -48,11 +39,10 @@ const Sidebar: FC = () => {
       <TagFilters />
       <TrackerFilters />
       <DiskUsage />
-      <BottomContainer>
-        <SidebarActions>
-          <ThemeSwitchButton />
-        </SidebarActions>
-      </BottomContainer>
+      <div style={{flexGrow: 1}} />
+      <SidebarActions>
+        <ThemeSwitchButton />
+      </SidebarActions>
     </OverlayScrollbarsComponent>
   );
 };

client/src/sass/components/_sidebar.scss
@@ -64,6 +64,7 @@
   }
 
   &__actions {
+    min-height: max-content;
     display: flex;
     padding: $spacing-unit * 1/5;
     justify-content: flex-start;