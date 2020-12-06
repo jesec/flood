jesec/flood

commit ebb4927b747bc244bfef28e5dd2d06896628fc44

server: torrents: ensure path is allowed when mediainfo is requested

server/routes/api/torrents.ts
@@ -668,6 +668,9 @@ router.get('/:hash/mediainfo', async (req, res) => {
   }
 
   const contentPath = fs.existsSync(path.join(directory, name)) ? path.join(directory, name) : directory;
+  if (!isAllowedPath(contentPath)) {
+    callback(null, accessDeniedError());
+  }
 
   try {
     const mediainfoProcess = childProcess.execFile(

commit 57e62273105bb2477782437f853a347fe6103c9b

server: torrents: returns when access denied in /mediainfo
Fixes: ebb4927b

server/routes/api/torrents.ts
@@ -679,6 +679,7 @@ router.get(
     const contentPath = fs.existsSync(path.join(directory, name)) ? path.join(directory, name) : directory;
     if (!isAllowedPath(contentPath)) {
       callback(null, accessDeniedError());
+      return;
     }
 
     try {