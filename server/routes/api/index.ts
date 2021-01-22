ba0cf7ea121d6c75a50d4eb8a720ff8387b3195b
Jesse Chan <jc@linux.com>
2021-01-22 23:39:38 +0800

    server: redirect to a portable link for /data endpoint

--- server/routes/api/index.ts
+++ server/routes/api/index.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import passport from 'passport';
+import rateLimit from 'express-rate-limit';
+
+import {contentTokenSchema} from '@shared/schema/api/torrents';
 
 import type {FloodSettings} from '@shared/types/FloodSettings';
 import type {HistorySnapshot} from '@shared/constants/historySnapshotTypes';
@@ -12,6 +15,7 @@ import clientRoutes from './client';
 import clientActivityStream from '../../middleware/clientActivityStream';
 import eventStream from '../../middleware/eventStream';
 import feedMonitorRoutes from './feed-monitor';
+import {getAuthToken, verifyToken} from '../../util/authUtil';
 import {getDirectoryList} from '../../util/fileUtil';
 import {getResponseFn} from '../../util/ajaxUtil';
 import torrentsRoutes from './torrents';
@@ -19,6 +23,51 @@ import torrentsRoutes from './torrents';
 const router = express.Router();
 
 router.use('/auth', authRoutes);
+
+// Special routes that may bypass authentication when conditions matched
+
+/**
+ * GET /api/torrents/{hash}/contents/{indices}/data
+ * @summary Gets downloaded data of contents of a torrent. Allows unauthenticated
+ *          access if a valid content token is found in the query.
+ * @see torrents.ts
+ */
+router.get<{hash: string; indices: string}, unknown, unknown, {token: string}>(
+  '/torrents/:hash/contents/:indices/data',
+  rateLimit({
+    windowMs: 5 * 60 * 1000,
+    max: 60,
+  }),
+  async (req, _res, next) => {
+    const {token} = req.query;
+
+    if (typeof token === 'string' && token !== '') {
+      const payload = await verifyToken(token).catch(() => undefined);
+
+      if (payload != null) {
+        const parsedResult = contentTokenSchema.safeParse(payload);
+
+        if (parsedResult.success) {
+          const {username, hash: authorizedHash, indices: authorizedIndices, iat} = parsedResult.data;
+
+          if (
+            typeof username === 'string' &&
+            typeof authorizedHash === 'string' &&
+            typeof authorizedIndices === 'string'
+          ) {
+            const {hash: requestedHash, indices: requestedIndices} = req.params;
+
+            if (requestedHash === authorizedHash && requestedIndices === authorizedIndices) {
+              req.cookies = {jwt: getAuthToken(username, iat)};
+            }
+          }
+        }
+      }
+    }
+
+    next();
+  },
+);
 
 // All subsequent routes need authentication
 router.use('/', passport.authenticate('jwt', {session: false}), appendUserServices);
--- server/util/authUtil.ts
+++ server/util/authUtil.ts
@@ -1,6 +1,8 @@
 import {CookieOptions} from 'express';
 import jwt from 'jsonwebtoken';
 
+import type {AuthToken} from '@shared/schema/Auth';
+
 import config from '../../config';
 
 const EXPIRATION_SECONDS = 60 * 60 * 24 * 7; // one week
@@ -11,12 +13,21 @@ export const getCookieOptions = (): CookieOptions => ({
   sameSite: 'strict',
 });
 
-export const getAuthToken = (username: string): string =>
-  jwt.sign({username}, config.secret, {
+export const getAuthToken = (username: string, iat?: number): string => {
+  const authTokenPayload: Partial<AuthToken> = {
+    username,
+  };
+
+  if (iat != null) {
+    authTokenPayload.iat = iat;
+  }
+
+  return jwt.sign(authTokenPayload, config.secret, {
     expiresIn: EXPIRATION_SECONDS,
   });
+};
 
-export const getToken = (payload: Record<string, unknown>) =>
+export const getToken = <T extends Record<string, unknown>>(payload: Omit<T, 'iat' | 'exp'>) =>
   jwt.sign(payload, config.secret, {
     expiresIn: EXPIRATION_SECONDS,
   });
