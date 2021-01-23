c5af49486517a4951fb7f8f16c150056e4d19b95
Jesse Chan <jc@linux.com>
2021-01-23 21:35:26 +0800

    TorrentMediainfo: migrate to Functional Component

--- client/src/javascript/components/modals/torrent-details-modal/TorrentMediainfo.tsx
+++ client/src/javascript/components/modals/torrent-details-modal/TorrentMediainfo.tsx
@@ -1,154 +1,96 @@
-import axios from 'axios';
-import Clipboard from 'clipboard';
-import {Component} from 'react';
-import {defineMessages, FormattedMessage, injectIntl, WrappedComponentProps} from 'react-intl';
+import axios, {CancelTokenSource} from 'axios';
+import {FC, useEffect, useRef, useState} from 'react';
+import {FormattedMessage, useIntl} from 'react-intl';
 
 import {Button} from '@client/ui';
-import {Clipboard as ClipboardIcon} from '@client/ui/icons';
+import {Checkmark, Clipboard} from '@client/ui/icons';
 import TorrentActions from '@client/actions/TorrentActions';
 import UIStore from '@client/stores/UIStore';
 
 import Tooltip from '../../general/Tooltip';
 
-const MESSAGES = defineMessages({
-  copy: {
-    id: 'general.clipboard.copy',
-  },
-  copied: {
-    id: 'general.clipboard.copied',
-  },
-  execError: {
-    id: 'mediainfo.execError',
-  },
-  fetching: {
-    id: 'mediainfo.fetching',
-  },
-  heading: {
-    id: 'mediainfo.heading',
-  },
-});
+const TorrentMediainfo: FC = () => {
+  const intl = useIntl();
+  const cancelToken = useRef<CancelTokenSource>(axios.CancelToken.source());
+  const clipboardRef = useRef<HTMLInputElement>(null);
+  const [mediainfo, setMediainfo] = useState<string | null>(null);
+  const [fetchMediainfoError, setFetchMediainfoError] = useState<Error | null>(null);
+  const [isFetchingMediainfo, setIsFetchingMediainfo] = useState<boolean>(true);
+  const [isCopiedToClipboard, setIsCopiedToClipboard] = useState<boolean>(false);
 
-interface TorrentMediainfoStates {
-  copiedToClipboard: boolean;
-}
-
-class TorrentMediainfo extends Component<WrappedComponentProps, TorrentMediainfoStates> {
-  mediainfo: string | null = null;
-  isFetchingMediainfo = true;
-  fetchMediainfoError: Error | null = null;
-
-  cancelToken = axios.CancelToken.source();
-  clipboard: Clipboard | null = null;
-  copyButtonRef: HTMLButtonElement | null = null;
-  timeoutId: NodeJS.Timeout | null = null;
-
-  constructor(props: WrappedComponentProps) {
-    super(props);
-
-    this.state = {
-      copiedToClipboard: false,
-    };
+  useEffect(() => {
+    const {current: currentCancelToken} = cancelToken;
 
     if (UIStore.activeModal?.id === 'torrent-details') {
-      TorrentActions.fetchMediainfo(UIStore.activeModal?.hash, this.cancelToken.token).then(
-        (mediainfo) => {
-          this.fetchMediainfoError = null;
-          this.mediainfo = mediainfo.output;
-          this.isFetchingMediainfo = false;
-          this.forceUpdate();
+      TorrentActions.fetchMediainfo(UIStore.activeModal?.hash, cancelToken.current.token).then(
+        (fetchedMediainfo) => {
+          setMediainfo(fetchedMediainfo.output);
+          setIsFetchingMediainfo(false);
         },
         (error) => {
           if (!axios.isCancel(error)) {
-            this.fetchMediainfoError = error.response.data;
-            this.isFetchingMediainfo = false;
-            this.forceUpdate();
+            setFetchMediainfoError(error.response.data);
+            setIsFetchingMediainfo(false);
           }
         },
       );
     }
+
+    return () => {
+      currentCancelToken.cancel();
+    };
+  }, []);
+
+  let headingMessageId = 'mediainfo.heading';
+  if (isFetchingMediainfo) {
+    headingMessageId = 'mediainfo.fetching';
+  } else if (fetchMediainfoError) {
+    headingMessageId = 'mediainfo.execError';
   }
 
-  componentDidUpdate() {
-    if (this.mediainfo === null) {
-      return;
-    }
-
-    if (this.copyButtonRef && this.clipboard == null) {
-      this.clipboard = new Clipboard(this.copyButtonRef, {
-        text: () => this.mediainfo as string,
-      });
-
-      this.clipboard.on('success', this.handleCopySuccess);
-    }
-  }
-
-  componentWillUnmount() {
-    this.cancelToken.cancel();
-    if (this.timeoutId != null) {
-      global.clearTimeout(this.timeoutId);
-    }
-  }
-
-  handleCopyButtonMouseLeave = () => {
-    this.timeoutId = global.setTimeout(() => {
-      this.setState({
-        copiedToClipboard: false,
-      });
-    }, 500);
-  };
-
-  handleCopySuccess = () => {
-    this.setState({
-      copiedToClipboard: true,
-    });
-  };
-
-  render() {
-    const {intl} = this.props;
-
-    let headingMessage = MESSAGES.heading;
-    if (this.isFetchingMediainfo) {
-      headingMessage = MESSAGES.fetching;
-    } else if (this.fetchMediainfoError) {
-      headingMessage = MESSAGES.execError;
-    }
-
-    let tooltipMessage = MESSAGES.copy;
-    if (this.state.copiedToClipboard) {
-      tooltipMessage = MESSAGES.copied;
-    }
-
-    return (
-      <div className="torrent-details__section mediainfo modal__content--nested-scroll__content">
-        <div className="mediainfo__toolbar">
-          <div className="mediainfo__toolbar__item">
-            <span className="torrent-details__table__heading--tertiary">
-              <FormattedMessage id={headingMessage.id} />
-            </span>
-          </div>
-          {this.isFetchingMediainfo || this.fetchMediainfoError ? null : (
-            <Tooltip
-              content={intl.formatMessage(tooltipMessage)}
-              onMouseLeave={this.handleCopyButtonMouseLeave}
-              wrapperClassName="tooltip__wrapper mediainfo__toolbar__item">
-              <Button
-                priority="tertiary"
-                buttonRef={(ref) => {
-                  this.copyButtonRef = ref;
-                }}>
-                <ClipboardIcon />
-              </Button>
-            </Tooltip>
-          )}
+  return (
+    <div className="torrent-details__section mediainfo modal__content--nested-scroll__content">
+      <div className="mediainfo__toolbar">
+        <div className="mediainfo__toolbar__item">
+          <span className="torrent-details__table__heading--tertiary">
+            <FormattedMessage id={headingMessageId} />
+          </span>
         </div>
-        {this.fetchMediainfoError ? (
-          <pre className="mediainfo__output mediainfo__output--error">{this.fetchMediainfoError.message}</pre>
-        ) : (
-          <pre className="mediainfo__output">{this.mediainfo}</pre>
+        {mediainfo && (
+          <Tooltip
+            content={intl.formatMessage({
+              id: isCopiedToClipboard ? 'general.clipboard.copied' : 'general.clipboard.copy',
+            })}
+            wrapperClassName="tooltip__wrapper mediainfo__toolbar__item">
+            <Button
+              priority="tertiary"
+              onClick={() => {
+                if (mediainfo != null) {
+                  if (typeof navigator.clipboard?.writeText === 'function') {
+                    navigator.clipboard.writeText(mediainfo).then(() => {
+                      setIsCopiedToClipboard(true);
+                    });
+                  } else if (clipboardRef.current != null) {
+                    clipboardRef.current.value = mediainfo;
+                    clipboardRef.current.select();
+                    document.execCommand('copy');
+                    setIsCopiedToClipboard(true);
+                  }
+                }
+              }}>
+              {isCopiedToClipboard ? <Checkmark /> : <Clipboard />}
+            </Button>
+          </Tooltip>
         )}
       </div>
-    );
-  }
-}
+      <input ref={clipboardRef} style={{width: '0.1px', height: '1px', position: 'absolute', right: 0}} />
+      {fetchMediainfoError ? (
+        <pre className="mediainfo__output mediainfo__output--error">{fetchMediainfoError.message}</pre>
+      ) : (
+        <pre className="mediainfo__output">{mediainfo}</pre>
+      )}
+    </div>
+  );
+};
 
-export default injectIntl(TorrentMediainfo);
+export default TorrentMediainfo;
