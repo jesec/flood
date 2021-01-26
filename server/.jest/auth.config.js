2daa77e883417adcdada5c26aa4d42947dafdaf5
Jesse Chan <jc@linux.com>
2021-01-26 08:24:22 +0800

    server: support path aliases in development

--- server/.jest/auth.config.js
+++ server/.jest/auth.config.js
@@ -1,7 +1,13 @@
+const {compilerOptions} = require('../tsconfig.json');
+const {pathsToModuleNameMapper} = require('ts-jest/utils');
+
 module.exports = {
   displayName: 'auth',
   preset: 'ts-jest/presets/js-with-babel',
   rootDir: './../',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/..',
+  }),
   testEnvironment: 'node',
   testMatch: ['<rootDir>/routes/api/auth.test.ts'],
   setupFilesAfterEnv: ['<rootDir>/.jest/auth.setup.js'],
--- server/.jest/rtorrent.config.js
+++ server/.jest/rtorrent.config.js
@@ -1,7 +1,13 @@
+const {compilerOptions} = require('../tsconfig.json');
+const {pathsToModuleNameMapper} = require('ts-jest/utils');
+
 module.exports = {
   displayName: 'rtorrent',
   preset: 'ts-jest/presets/js-with-babel',
   rootDir: './../',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, {
+    prefix: '<rootDir>/..',
+  }),
   testEnvironment: 'node',
   testPathIgnorePatterns: ['auth.test.ts'],
   setupFilesAfterEnv: ['<rootDir>/.jest/rtorrent.setup.js'],
