afffb6d18ca1821ea336387ab8642b2a7d685e3f
Jesse Chan <jc@linux.com>
2021-03-07 01:10:59 +0800

    FilesystemBrowser: redesign to allow searching in current directory
    Plus, this greatly reduces the number of unnecessary requests sent
    to Flood server.

--- client/src/javascript/util/termMatch.ts
+++ client/src/javascript/util/termMatch.ts
@@ -1,23 +1,34 @@
-const termMatch = <T>(elements: Array<T>, sub: (element: T) => string, searchString: string): Array<T> => {
-  if (searchString !== '') {
+const termMatch = <T>(
+  elements: Array<T> | undefined | null,
+  sub: (element: T) => string,
+  searchString: string,
+): Array<T> => {
+  if (searchString !== '' && elements?.length) {
     const queries: Array<RegExp> = [];
     const searchTerms = searchString.replace(/,/g, ' ').split(' ');
 
     for (let i = 0, len = searchTerms.length; i < len; i += 1) {
-      queries.push(new RegExp(searchTerms[i], 'gi'));
+      try {
+        queries.push(new RegExp(searchTerms[i], 'gi'));
+      } catch {
+        // do nothing.
+      }
     }
 
     return elements.filter((element) => {
-      for (let i = 0, len = queries.length; i < len; i += 1) {
-        if (!sub(element).match(queries[i])) {
-          return false;
-        }
+      if (sub(element) === searchString) {
+        return true;
       }
-      return true;
+
+      if (queries.every((query) => sub(element).match(query))) {
+        return true;
+      }
+
+      return false;
     });
   }
 
-  return elements;
+  return elements ?? [];
 };
 
 export default termMatch;
