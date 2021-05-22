jesec/flood

commit 6f984a26a82d20d992fe2c43eb240de1a1aae296

config: forbid non-owner access to runtime directory by default

config.ts
@@ -203,8 +203,9 @@ process.on('SIGINT', () => {
 });
 
 try {
-  fs.mkdirSync(path.join(argv.rundir, 'db'), {recursive: true});
-  fs.mkdirSync(path.join(argv.rundir, 'temp'), {recursive: true});
+  fs.mkdirSync(path.join(argv.rundir), {recursive: true, mode: 0o700});
+  fs.mkdirSync(path.join(argv.rundir, 'db'));
+  fs.mkdirSync(path.join(argv.rundir, 'temp'));
 } catch (error) {
   console.error('Failed to access runtime directory');
   process.exit(1);

commit 2c0e036c0251a18b5c41aabaae1d4cd6d555828e

config: always mkdir with "recursive"
Fixes: 6f984a26a82d20d992fe2c43eb240de1a1aae296

config.ts
@@ -204,8 +204,8 @@ process.on('SIGINT', () => {
 
 try {
   fs.mkdirSync(path.join(argv.rundir), {recursive: true, mode: 0o700});
-  fs.mkdirSync(path.join(argv.rundir, 'db'));
-  fs.mkdirSync(path.join(argv.rundir, 'temp'));
+  fs.mkdirSync(path.join(argv.rundir, 'db'), {recursive: true});
+  fs.mkdirSync(path.join(argv.rundir, 'temp'), {recursive: true});
 } catch (error) {
   console.error('Failed to access runtime directory');
   process.exit(1);