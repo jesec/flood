ae5f7af30aa317687209f348056b5539a1bac772
Jesse Chan <jc@linux.com>
2021-06-21 19:38:19 +0800

    server: settingService: convert to "nedb-promises"

--- server/services/settingService.ts
+++ server/services/settingService.ts
@@ -1,4 +1,4 @@
-import Datastore from 'nedb';
+import Datastore from 'nedb-promises';
 import path from 'path';
 
 import type {FloodSettings} from '@shared/types/FloodSettings';
@@ -16,87 +16,48 @@ interface SettingServiceEvents {
 }
 
 class SettingService extends BaseService<SettingServiceEvents> {
-  db = this.loadDatabase();
-
-  loadDatabase(): Datastore<SettingRecord> {
-    const userId = this.user._id;
-
-    const database = new Datastore<SettingRecord>({
-      autoload: true,
-      filename: path.join(config.dbPath, userId, 'settings', 'settings.db'),
-    });
-
-    return database;
-  }
+  db = Datastore.create({
+    autoload: true,
+    filename: path.join(config.dbPath, this.user._id, 'settings', 'settings.db'),
+  });
 
   async get(property: keyof FloodSettings | null): Promise<Partial<FloodSettings>> {
-    return new Promise((resolve, reject) => {
-      this.db
-        .find(
-          property
-            ? {
-                id: property,
-              }
-            : {},
-        )
-        .exec(async (err, docs) => {
-          if (err) {
-            reject(err);
-          }
+    const docs = await this.db
+      .find<SettingRecord>(
+        property
+          ? {
+              id: property,
+            }
+          : {},
+      )
+      .exec();
 
-          resolve(
-            Object.assign(
-              {},
-              ...(await Promise.all(
-                docs.map(async (doc) => {
-                  return {
-                    [doc.id]: doc.data,
-                  };
-                }),
-              )),
-            ),
-          );
-        });
-    });
+    return Object.assign(
+      {},
+      ...docs.map((doc) => {
+        return {
+          [doc.id]: doc.data,
+        };
+      }),
+    );
   }
 
   async set(changedSettings: Partial<FloodSettings>): Promise<Partial<FloodSettings>> {
     const savedSettings: typeof changedSettings = {};
 
     if (changedSettings) {
-      let error: Error | null = null;
-
       await Promise.all(
-        Object.keys(changedSettings).map((key) => {
-          return new Promise<keyof FloodSettings>((resolve, reject) => {
-            const property = key as keyof FloodSettings;
-            return this.db.update(
-              {id: property},
-              {$set: {data: changedSettings[property]}},
-              {upsert: true},
-              (err: Error | null) => {
-                if (err) {
-                  reject(err);
-                  return;
-                }
-                resolve(property);
-              },
-            );
-          })
-            .then((property) => {
-              Object.assign(savedSettings, {
-                [property]: changedSettings[property],
-              });
-            })
-            .catch((e) => {
-              error = e;
-            });
+        Object.keys(changedSettings).map(async (key) => {
+          const property = key as keyof FloodSettings;
+          const value = changedSettings[property];
+
+          await this.db.update<SettingRecord>({id: property}, {$set: {data: value}}, {upsert: true});
+
+          Object.assign(savedSettings, {
+            [property]: value,
+          });
         }),
       );
-
-      if (error) {
-        return Promise.reject(error);
-      }
     }
 
     this.emit('SETTINGS_CHANGE', savedSettings);
