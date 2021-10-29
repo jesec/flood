jesec/flood

commit 916b71b1171fcc4d7e82656d9e4c7969b5f51583

client: remove unnecessary "UIActions" abstraction

client/src/javascript/components/sidebar/TagFilters.tsx
@@ -4,7 +4,6 @@ import {useLingui} from '@lingui/react';
 
 import SidebarFilter from './SidebarFilter';
 import TorrentFilterStore from '../../stores/TorrentFilterStore';
-import UIActions from '../../actions/UIActions';
 
 const TagFilters: FC = observer(() => {
   const {i18n} = useLingui();
@@ -29,7 +28,7 @@ const TagFilters: FC = observer(() => {
 
   const filterElements = filterItems.map((filter) => (
     <SidebarFilter
-      handleClick={UIActions.setTorrentTagFilter}
+      handleClick={TorrentFilterStore.setTagFilter}
       count={TorrentFilterStore.taxonomy.tagCounts[filter] || 0}
       key={filter}
       isActive={filter === TorrentFilterStore.filters.tagFilter}

client/src/javascript/components/sidebar/TrackerFilters.tsx
@@ -4,7 +4,6 @@ import {useLingui} from '@lingui/react';
 
 import SidebarFilter from './SidebarFilter';
 import TorrentFilterStore from '../../stores/TorrentFilterStore';
-import UIActions from '../../actions/UIActions';
 
 const TrackerFilters: FC = observer(() => {
   const {i18n} = useLingui();
@@ -28,7 +27,7 @@ const TrackerFilters: FC = observer(() => {
 
   const filterElements = filterItems.map((filter) => (
     <SidebarFilter
-      handleClick={UIActions.setTorrentTrackerFilter}
+      handleClick={TorrentFilterStore.setTrackerFilter}
       count={TorrentFilterStore.taxonomy.trackerCounts[filter] || 0}
       key={filter}
       isActive={filter === TorrentFilterStore.filters.trackerFilter}

client/src/javascript/actions/UIActions.ts
@@ -1,57 +1,0 @@
-import {KeyboardEvent, MouseEvent, TouchEvent} from 'react';
-
-import type {TorrentStatus} from '@shared/constants/torrentStatusMap';
-
-import TorrentFilterStore from '../stores/TorrentFilterStore';
-import TorrentStore from '../stores/TorrentStore';
-import UIStore from '../stores/UIStore';
-
-import type {ActiveContextMenu, Modal} from '../stores/UIStore';
-
-const UIActions = {
-  displayContextMenu: (contextMenu: ActiveContextMenu) => {
-    UIStore.setActiveContextMenu(contextMenu);
-  },
-
-  displayDropdownMenu: (id: string | null) => {
-    UIStore.setActiveDropdownMenu(id);
-  },
-
-  displayModal: (modal: Modal) => {
-    UIStore.setActiveModal(modal);
-  },
-
-  dismissContextMenu: (id: string) => {
-    UIStore.dismissContextMenu(id);
-  },
-
-  dismissModal: () => {
-    UIStore.dismissModal();
-  },
-
-  handleTorrentClick: (data: {event: KeyboardEvent | MouseEvent | TouchEvent; hash: string}) => {
-    TorrentStore.setSelectedTorrents(data);
-  },
-
-  selectAllTorrents: () => {
-    TorrentStore.selectAllTorrents();
-  },
-
-  setTorrentStatusFilter: (status: TorrentStatus) => {
-    TorrentFilterStore.setStatusFilter(status);
-  },
-
-  setTorrentTagFilter: (tag: string) => {
-    TorrentFilterStore.setTagFilter(tag);
-  },
-
-  setTorrentTrackerFilter: (tracker: string) => {
-    TorrentFilterStore.setTrackerFilter(tracker);
-  },
-
-  setTorrentsSearchFilter: (search: string) => {
-    TorrentFilterStore.setSearchFilter(search);
-  },
-} as const;
-
-export default UIActions;

client/src/javascript/components/general/ContextMenuMountPoint.tsx
@@ -5,7 +5,6 @@ import {useLingui} from '@lingui/react';
 import {useKeyPressEvent} from 'react-use';
 
 import {ContextMenu} from '@client/ui';
-import UIActions from '@client/actions/UIActions';
 import UIStore from '@client/stores/UIStore';
 
 import type {ActiveContextMenu} from '@client/stores/UIStore';
@@ -24,13 +23,13 @@ const ContextMenuMountPoint: FC<ContextMenuMountPointProps> = observer(({id}: Co
 
   const {i18n} = useLingui();
 
-  useKeyPressEvent('Escape', () => UIActions.dismissContextMenu(id));
+  useKeyPressEvent('Escape', () => UIStore.dismissContextMenu(id));
 
   return (
     <ContextMenu
       triggerCoordinates={triggerCoordinates}
       onOverlayClick={() => {
-        UIActions.dismissContextMenu(id);
+        UIStore.dismissContextMenu(id);
       }}
       onOverlayRightClick={(e) => {
         e.preventDefault();
@@ -92,7 +91,7 @@ const ContextMenuMountPoint: FC<ContextMenuMountPointProps> = observer(({id}: Co
                   }
 
                   if (item.dismissMenu !== false) {
-                    UIActions.dismissContextMenu(id);
+                    UIStore.dismissContextMenu(id);
                   }
                 }
 

client/src/javascript/components/general/form-elements/Dropdown.tsx
@@ -5,8 +5,7 @@ import {observer} from 'mobx-react';
 import uniqueId from 'lodash/uniqueId';
 import {useKeyPressEvent} from 'react-use';
 
-import UIActions from '../../../actions/UIActions';
-import UIStore from '../../../stores/UIStore';
+import UIStore from '@client/stores/UIStore';
 
 interface DropdownButtonProps {
   className?: string;
@@ -101,7 +100,7 @@ const Dropdown = observer(
     const closeDropdown = useCallback(() => {
       window.removeEventListener('click', closeDropdown);
 
-      UIActions.displayDropdownMenu(null);
+      UIStore.setActiveDropdownMenu(null);
     }, []);
 
     useKeyPressEvent('Escape', () => closeDropdown());
@@ -113,7 +112,7 @@ const Dropdown = observer(
         onOpen();
       }
 
-      UIActions.displayDropdownMenu(id.current);
+      UIStore.setActiveDropdownMenu(id.current);
     }, [closeDropdown, onOpen]);
 
     const handleDropdownClick = (event?: SyntheticEvent): void => {

client/src/javascript/components/modals/ModalActions.tsx
@@ -1,7 +1,7 @@
 import {FC} from 'react';
 
 import {Button, Checkbox} from '@client/ui';
-import UIActions from '@client/actions/UIActions';
+import UIStore from '@client/stores/UIStore';
 
 import type {ModalAction} from '@client/stores/UIStore';
 
@@ -19,7 +19,7 @@ const ModalActions: FC<ModalActionsProps> = (props: ModalActionsProps) => {
 
     if (action.triggerDismiss) {
       dismissIfNeeded = () => {
-        UIActions.dismissModal();
+        UIStore.setActiveModal(null);
       };
     }
 

client/src/javascript/components/modals/Modals.tsx
@@ -13,7 +13,6 @@ import SetTagsModal from './set-tags-modal/SetTagsModal';
 import SetTrackersModal from './set-trackers-modal/SetTrackersModal';
 import SettingsModal from './settings-modal/SettingsModal';
 import TorrentDetailsModal from './torrent-details-modal/TorrentDetailsModal';
-import UIActions from '../../actions/UIActions';
 import UIStore from '../../stores/UIStore';
 
 import type {Modal} from '../../stores/UIStore';
@@ -48,7 +47,7 @@ const createModal = (id: Modal['id']): React.ReactNode => {
 const Modals: FC = observer(() => {
   const {id} = UIStore.activeModal || {};
 
-  useKeyPressEvent('Escape', () => UIActions.dismissModal());
+  useKeyPressEvent('Escape', () => UIStore.setActiveModal(null));
 
   let modal;
   if (id != null) {
@@ -59,7 +58,7 @@ const Modals: FC = observer(() => {
             className="modal__overlay"
             role="none"
             onClick={() => {
-              UIActions.dismissModal();
+              UIStore.setActiveModal(null);
             }}
           />
           {createModal(id)}

client/src/javascript/components/modals/add-torrents-modal/AddTorrentsByCreation.tsx
@@ -93,7 +93,7 @@ const AddTorrentsByCreation: FC = () => {
             start: formData.start || false,
             tags: formData.tags != null ? formData.tags.split(',') : undefined,
           }).then(() => {
-            UIStore.dismissModal();
+            UIStore.setActiveModal(null);
           });
 
           saveAddTorrentsUserPreferences({

commit 0da58ffe47c8633477cb8afa685df8408fa7cea2

TagFilters, TrackerFilters: fix filtering by tag/tracker
Fixes: 916b71b1171fcc4d7e82656d9e4c7969b5f51583

client/src/javascript/components/sidebar/TagFilters.tsx
@@ -28,7 +28,7 @@ const TagFilters: FC = observer(() => {
 
   const filterElements = filterItems.map((filter) => (
     <SidebarFilter
-      handleClick={TorrentFilterStore.setTagFilter}
+      handleClick={(tag) => TorrentFilterStore.setTagFilter(tag)}
       count={TorrentFilterStore.taxonomy.tagCounts[filter] || 0}
       key={filter}
       isActive={filter === TorrentFilterStore.filters.tagFilter}

client/src/javascript/components/sidebar/TrackerFilters.tsx
@@ -27,7 +27,7 @@ const TrackerFilters: FC = observer(() => {
 
   const filterElements = filterItems.map((filter) => (
     <SidebarFilter
-      handleClick={TorrentFilterStore.setTrackerFilter}
+      handleClick={(tracker) => TorrentFilterStore.setTrackerFilter(tracker)}
       count={TorrentFilterStore.taxonomy.trackerCounts[filter] || 0}
       key={filter}
       isActive={filter === TorrentFilterStore.filters.trackerFilter}