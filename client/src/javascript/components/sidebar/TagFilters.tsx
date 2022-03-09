jesec/flood

commit e368cac4fd0af4c4aeedad6bc609d66b97e60eb7

sidebar: make filter sections collapsible (#519)
Co-authored-by: FinalDoom <7464170-FinalDoom@users.noreply.gitlab.com>

client/src/javascript/components/sidebar/TagFilters.tsx
@@ -1,8 +1,9 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import {observer} from 'mobx-react';
 import {useLingui} from '@lingui/react';
 
 import SidebarFilter from './SidebarFilter';
+import Expando from './Expando';
 import TorrentFilterStore from '../../stores/TorrentFilterStore';
 
 const TagFilters: FC = observer(() => {
@@ -42,12 +43,19 @@ const TagFilters: FC = observer(() => {
 
   const title = i18n._('filter.tag.title');
 
+  const [expanded, setExpanded] = useState<boolean>(true);
+  const expandoClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <ul aria-label={title} className="sidebar-filter sidebar__item" role="menu">
-      <li className="sidebar-filter__item sidebar-filter__item--heading" role="none">
-        {title}
+      <li className="sidebar-filter__item" role="none">
+        <Expando className="sidebar-filter__item--heading" expanded={expanded} handleClick={expandoClick}>
+          {title}
+        </Expando>
       </li>
-      {filterElements}
+      {expanded && filterElements}
     </ul>
   );
 });

client/src/javascript/components/sidebar/TrackerFilters.tsx
@@ -1,8 +1,9 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import {observer} from 'mobx-react';
 import {useLingui} from '@lingui/react';
 
 import SidebarFilter from './SidebarFilter';
+import Expando from './Expando';
 import TorrentFilterStore from '../../stores/TorrentFilterStore';
 
 const TrackerFilters: FC = observer(() => {
@@ -41,12 +42,19 @@ const TrackerFilters: FC = observer(() => {
 
   const title = i18n._('filter.tracker.title');
 
+  const [expanded, setExpanded] = useState<boolean>(true);
+  const expandoClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <ul aria-label={title} className="sidebar-filter sidebar__item" role="menu">
-      <li className="sidebar-filter__item sidebar-filter__item--heading" role="none">
-        {title}
+      <li className="sidebar-filter__item" role="none">
+        <Expando className="sidebar-filter__item--heading" expanded={expanded} handleClick={expandoClick}>
+          {title}
+        </Expando>
       </li>
-      {filterElements}
+      {expanded && filterElements}
     </ul>
   );
 });

client/src/javascript/components/sidebar/Expando.tsx
@@ -1,0 +1,22 @@
+import {FC, KeyboardEvent, MouseEvent, ReactNode, TouchEvent} from 'react';
+import {Chevron} from '@client/ui/icons';
+
+interface ExpandoProps {
+  children: ReactNode;
+  className?: string;
+  expanded: boolean;
+  handleClick: (event: KeyboardEvent | MouseEvent | TouchEvent) => void;
+}
+
+const Expando: FC<ExpandoProps> = ({children, className, expanded, handleClick}: ExpandoProps) => (
+  <button className={className} onClick={(event) => handleClick(event)}>
+    {children}
+    {expanded ? <Chevron css={{transform: 'scaleY(-1)'}} /> : <Chevron />}
+  </button>
+);
+
+Expando.defaultProps = {
+  className: undefined,
+};
+
+export default Expando;

client/src/javascript/components/sidebar/StatusFilters.tsx
@@ -1,4 +1,4 @@
-import {FC} from 'react';
+import {FC, useState} from 'react';
 import {observer} from 'mobx-react';
 import {useLingui} from '@lingui/react';
 
@@ -8,6 +8,7 @@ import TorrentFilterStore from '@client/stores/TorrentFilterStore';
 import type {TorrentStatus} from '@shared/constants/torrentStatusMap';
 
 import SidebarFilter from './SidebarFilter';
+import Expando from './Expando';
 
 const StatusFilters: FC = observer(() => {
   const {i18n} = useLingui();
@@ -81,12 +82,19 @@ const StatusFilters: FC = observer(() => {
 
   const title = i18n._('filter.status.title');
 
+  const [expanded, setExpanded] = useState<boolean>(true);
+  const expandoClick = () => {
+    setExpanded(!expanded);
+  };
+
   return (
     <ul aria-label={title} className="sidebar-filter sidebar__item" role="menu">
-      <li className="sidebar-filter__item sidebar-filter__item--heading" role="none">
-        {title}
+      <li className="sidebar-filter__item" role="none">
+        <Expando className="sidebar-filter__item--heading" expanded={expanded} handleClick={expandoClick}>
+          {title}
+        </Expando>
       </li>
-      {filterElements}
+      {expanded && filterElements}
     </ul>
   );
 });

commit 33318612ff53bb47a0bbc81d305353a0368a96cc

client: Fix regression introduced in PR #519, fixes #522

client/src/javascript/components/sidebar/TagFilters.tsx
@@ -8,6 +8,7 @@ import TorrentFilterStore from '../../stores/TorrentFilterStore';
 
 const TagFilters: FC = observer(() => {
   const {i18n} = useLingui();
+  const [expanded, setExpanded] = useState<boolean>(true);
 
   const tags = Object.keys(TorrentFilterStore.taxonomy.tagCounts);
 
@@ -43,7 +44,6 @@ const TagFilters: FC = observer(() => {
 
   const title = i18n._('filter.tag.title');
 
-  const [expanded, setExpanded] = useState<boolean>(true);
   const expandoClick = () => {
     setExpanded(!expanded);
   };

client/src/javascript/components/sidebar/TrackerFilters.tsx
@@ -8,6 +8,7 @@ import TorrentFilterStore from '../../stores/TorrentFilterStore';
 
 const TrackerFilters: FC = observer(() => {
   const {i18n} = useLingui();
+  const [expanded, setExpanded] = useState<boolean>(true);
 
   const trackers = Object.keys(TorrentFilterStore.taxonomy.trackerCounts);
 
@@ -42,7 +43,6 @@ const TrackerFilters: FC = observer(() => {
 
   const title = i18n._('filter.tracker.title');
 
-  const [expanded, setExpanded] = useState<boolean>(true);
   const expandoClick = () => {
     setExpanded(!expanded);
   };