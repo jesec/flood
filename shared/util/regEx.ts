37391d62e5387bd1e0bfe78228d75bdb03295a96
Fabricio Augusto <fabricionaweb@gmail.com>
2022-03-22 22:18:58 +0000

    magnet: add shared regex to magnet urls

--- shared/util/regEx.ts
+++ shared/util/regEx.ts
@@ -1,3 +1,4 @@
 export const url = /^(?:https?|ftp):\/\/.{1,}\.{1}.{1,}/;
+export const magnet = /^magnet:\?xt=urn:[a-z0-9]+:[a-z0-9]{32,40}&dn=.+&tr=.+$/;
 export const cdata = /<!\[CDATA\[(.*?)\]\]>/;
 export const noComma = /^[^,]+$/;
