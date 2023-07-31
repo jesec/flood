2d8c97c8b2da37a3876b3fdab5953df51b53d278
Trim21 <trim21.me@gmail.com>
2023-07-31 10:51:57 +0800

    revert unexpected code

--- server/bin/index.ts
+++ server/bin/index.ts
@@ -7,9 +7,6 @@ import migrateData from './migrations/run';
 import startWebServer from './web-server';
 
 if (process.env.NODE_ENV !== 'development') {
-  // Use production mode by default
-  process.env.NODE_ENV = 'production';
-
   // Catch unhandled rejections and exceptions
   // Traces are pretty useless with minimized production codes
   // This avoids printing a large section of junk
