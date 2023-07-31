5b63ed5af33a1b0588a3f8da2a5e5f9e964d7ff6
Trim21 <trim21.me@gmail.com>
2023-07-31 12:28:43 +0800

    disable test request logger

--- server/routes/api/auth.test.ts
+++ server/routes/api/auth.test.ts
@@ -37,7 +37,7 @@ const testNonAdminUser = {
 } as const;
 let testNonAdminUserToken = '';
 
-const app = fastify();
+const app = fastify({disableRequestLogging: true, logger: false});
 let request: supertest.SuperTest<supertest.Test>;
 
 beforeAll(async () => {
