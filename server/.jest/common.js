b835a9d75138f101fda4c11381609f39d40c9104
Trim21 <trim21.me@gmail.com>
2024-08-10 04:17:04 +0800

    update

--- server/.jest/common.js
+++ server/.jest/common.js
@@ -1,9 +1,8 @@
 module.exports = {
-  // need to match node_modules/.pnpm/p-queue@7.4.1/node_modules/p-queue/dist/index.js
-  transformIgnorePatterns: ['node_modules/.pnpm/(p-queue|p-timeout)[^/]*/.*'],
   transform: {
     // transform ESM only package to CommonJS
-    '^.+\\.(t|j)sx?$': [
+    // need to match node_modules/.pnpm/p-queue@7.4.1/node_modules/p-queue/dist/index.js
+    'node_modules/.pnpm/(p-queue|p-timeout)[^/]*/.*.(j|t)s': [
       'jest-esbuild',
       {
         format: 'cjs',
