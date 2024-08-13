7fe1f0792b4df65462f10690831f1719e2b3d11a
Trim21 <trim21.me@gmail.com>
2024-08-14 05:32:01 +0800

    update

--- scripts/update-mmdb.mjs
+++ scripts/update-mmdb.mjs
@@ -6,7 +6,11 @@ const buf = fs.readFileSync('server/geoip/GeoLite2-Country.mmdb');
 fs.writeFileSync(
   'server/geoip/data.mjs',
   `
+import * as zlib from 'node:zlib';
+
 // data is brotli compressed GeoLite2-Country.mmdb in base64 format
-export const data = "${zlib.brotliCompressSync(buf).toString('base64')}";
+export const data = zlib.brotliDecompressSync(Buffer.from('${zlib
+    .brotliCompressSync(buf)
+    .toString('base64')}', 'base64'));
 `,
 );
