50a33298c1474ff981d9cd045db767f14b016cd9
Trim21 <trim21.me@gmail.com>
2024-12-10 03:42:39 +0800

    add os and arch to binary name

--- scripts/build-binary.mjs
+++ scripts/build-binary.mjs
@@ -2,6 +2,7 @@
  * build single executable binary with Node.js SEA
  */
 
+import * as os from 'node:os';
 import * as fs from 'node:fs';
 import * as path from 'node:path/posix';
 import {execSync} from 'node:child_process';
@@ -11,6 +12,11 @@ async function main() {
   if (major <= 20) {
     console.error('need node20 to run this scripts');
     return;
+  }
+
+  let binaryName = `flood-${os.platform()}-${os.arch()}`;
+  if (os.platform() == 'win32') {
+    binaryName += '.exe';
   }
 
   fs.mkdirSync('dist-pkg', {recursive: true});
@@ -23,9 +29,9 @@ async function main() {
   fs.writeFileSync('sea-config.json', JSON.stringify({...data, assets}));
 
   execSync('node --experimental-sea-config sea-config.json');
-  fs.copyFileSync(process.execPath, 'dist-pkg/flood');
+  fs.copyFileSync(process.execPath, 'dist-pkg/' + binaryName);
   execSync(
-    'npx postject dist-pkg/flood NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2',
+    `npx postject dist-pkg/${binaryName} NODE_SEA_BLOB sea-prep.blob --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2`,
   );
 }
 
