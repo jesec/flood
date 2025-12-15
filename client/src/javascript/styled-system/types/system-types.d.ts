85ed14e66239cbb592fde86e1b2cec1fbacdb693
Trim21 <trim21.me@gmail.com>
2025-12-16 01:49:05 +0800

    fix

--- client/src/javascript/styled-system/types/system-types.d.ts
+++ client/src/javascript/styled-system/types/system-types.d.ts
@@ -1,30 +1,30 @@
 /* eslint-disable */
-import type {  ConditionalValue, Nested  } from './conditions';
-import type {  AtRule, Globals, PropertiesFallback  } from './csstype';
-import type {  SystemProperties, CssVarProperties  } from './style-props';
+import type {ConditionalValue, Nested} from './conditions';
+import type {AtRule, Globals, PropertiesFallback} from './csstype';
+import type {SystemProperties, CssVarProperties} from './style-props';
 
-type String = string & {}
-type Number = number & {}
+type String = string & {};
+type Number = number & {};
 
-export type Pretty<T> = { [K in keyof T]: T[K] } & {}
+export type Pretty<T> = {[K in keyof T]: T[K]} & {};
 
-export type DistributiveOmit<T, K extends keyof any> = T extends unknown ? Omit<T, K> : never
+export type DistributiveOmit<T, K extends keyof any> = T extends unknown ? Omit<T, K> : never;
 
 export type DistributiveUnion<T, U> = {
-  [K in keyof T]: K extends keyof U ? U[K] | T[K] : T[K]
-} & DistributiveOmit<U, keyof T>
+  [K in keyof T]: K extends keyof U ? U[K] | T[K] : T[K];
+} & DistributiveOmit<U, keyof T>;
 
 export type Assign<T, U> = {
-  [K in keyof T]: K extends keyof U ? U[K] : T[K]
-} & U
+  [K in keyof T]: K extends keyof U ? U[K] : T[K];
+} & U;
 
 /* -----------------------------------------------------------------------------
  * Native css properties
  * -----------------------------------------------------------------------------*/
 
-type DashedIdent = `--${string}`
+type DashedIdent = `--${string}`;
 
-type StringToMultiple<T extends string> = T | `${T}, ${T}`
+type StringToMultiple<T extends string> = T | `${T}, ${T}`;
 
 export type PositionAreaAxis =
   | 'left'
@@ -74,7 +74,7 @@ export type PositionAreaAxis =
   | 'self-start'
   | 'self-end'
   | 'span-self-start'
-  | 'span-self-end'
+  | 'span-self-end';
 
 type PositionTry =
   | 'normal'
@@ -88,108 +88,108 @@ type PositionTry =
   | 'block-end'
   | 'inline-start'
   | 'inline-end'
-  | DashedIdent
+  | DashedIdent;
 
 export interface ModernCssProperties {
   /**
    * Defines a name for the anchor element that can be referenced by positioned elements.
    */
-  anchorName?: Globals | 'none' | DashedIdent | StringToMultiple<DashedIdent>
+  anchorName?: Globals | 'none' | DashedIdent | StringToMultiple<DashedIdent>;
 
   /**
    * Defines the scope of anchor names within the element.
    */
-  anchorScope?: Globals | 'none' | 'all' | DashedIdent | StringToMultiple<DashedIdent>
+  anchorScope?: Globals | 'none' | 'all' | DashedIdent | StringToMultiple<DashedIdent>;
 
   /**
    * Controls how form elements size themselves.
    */
-  fieldSizing?: Globals | 'fixed' | 'content'
+  fieldSizing?: Globals | 'fixed' | 'content';
 
   /**
    * Controls whether interpolation of size values should allow keywords.
    */
-  interpolateSize?: Globals | 'allow-keywords' | 'numeric-only'
+  interpolateSize?: Globals | 'allow-keywords' | 'numeric-only';
 
   /**
    * Specifies the anchor element that this positioned element should be positioned relative to.
    */
-  positionAnchor?: Globals | 'auto' | DashedIdent
+  positionAnchor?: Globals | 'auto' | DashedIdent;
 
   /**
    * Specifies the area within the anchor element where this positioned element should be placed.
    */
-  positionArea?: Globals | 'auto' | PositionAreaAxis | `${PositionAreaAxis} ${PositionAreaAxis}` | String
+  positionArea?: Globals | 'auto' | PositionAreaAxis | `${PositionAreaAxis} ${PositionAreaAxis}` | String;
 
   /**
    * Specifies the position try options for the element.
    */
-  positionTry?: Globals | StringToMultiple<PositionTry> | String
+  positionTry?: Globals | StringToMultiple<PositionTry> | String;
 
   /**
    * Specifies fallback position try options when the primary position fails.
    */
-  positionTryFallback?: Globals | 'none' | StringToMultiple<PositionTry> | String
+  positionTryFallback?: Globals | 'none' | StringToMultiple<PositionTry> | String;
 
   /**
    * Specifies the order in which position try options should be attempted.
    */
-  positionTryOrder?: Globals | 'normal' | 'most-width' | 'most-height' | 'most-block-size' | 'most-inline-size'
+  positionTryOrder?: Globals | 'normal' | 'most-width' | 'most-height' | 'most-block-size' | 'most-inline-size';
 
   /**
    * Controls when the positioned element should be visible.
    */
-  positionVisibility?: Globals | 'always' | 'anchors-visible' | 'no-overflow'
+  positionVisibility?: Globals | 'always' | 'anchors-visible' | 'no-overflow';
 
   /**
    * Controls whether text should wrap or not.
    */
-  textWrapMode?: Globals | 'wrap' | 'nowrap'
+  textWrapMode?: Globals | 'wrap' | 'nowrap';
 
   /**
    * Controls trimming of spacing in text.
    */
-  textSpacingTrim?: Globals | 'normal' | 'space-all' | 'space-first' | 'trim-start'
+  textSpacingTrim?: Globals | 'normal' | 'space-all' | 'space-first' | 'trim-start';
 
   /**
    * Controls the style of text wrapping.
    */
-  textWrapStyle?: Globals | 'auto' | 'balance' | 'pretty' | 'stable'
+  textWrapStyle?: Globals | 'auto' | 'balance' | 'pretty' | 'stable';
 
   /**
    * Controls whether the entire element should be draggable instead of its contents.
    */
-  WebkitUserDrag?: Globals | 'auto' | 'element' | 'none'
+  WebkitUserDrag?: Globals | 'auto' | 'element' | 'none';
 
   /**
    * Specifies whether an element can be used to drag the entire app window (Electron).
    */
-  WebkitAppRegion?: Globals | 'drag' | 'no-drag'
+  WebkitAppRegion?: Globals | 'drag' | 'no-drag';
 
   /**
    * Sets the horizontal spacing between table borders.
    */
-  WebkitBorderHorizontalSpacing?: Globals | String | Number
+  WebkitBorderHorizontalSpacing?: Globals | String | Number;
 
   /**
    * Sets the vertical spacing between table borders.
    */
-  WebkitBorderVerticalSpacing?: Globals | String | Number
+  WebkitBorderVerticalSpacing?: Globals | String | Number;
 
   /**
    * Controls the display of text content for security purposes (e.g., password fields).
    */
-  WebkitTextSecurity?: Globals | 'none' | 'circle' | 'disc' | 'square'
+  WebkitTextSecurity?: Globals | 'none' | 'circle' | 'disc' | 'square';
 }
 
-export type CssProperty = keyof PropertiesFallback
+export type CssProperty = keyof PropertiesFallback;
 
 export interface CssProperties extends PropertiesFallback<String | Number>, CssVarProperties, ModernCssProperties {}
 
 export interface CssKeyframes {
   [name: string]: {
-    [time: string]: CssProperties
-  }
+    [time: string]: CssProperties;
+  };
 }
 
 /* -----------------------------------------------------------------------------
@@ -197,23 +197,23 @@ export interface CssKeyframes {
  * -----------------------------------------------------------------------------*/
 
 interface GenericProperties {
-  [key: string]: ConditionalValue<String | Number | boolean>
+  [key: string]: ConditionalValue<String | Number | boolean>;
 }
 
 /* -----------------------------------------------------------------------------
  * Native css props
  * -----------------------------------------------------------------------------*/
 
-export type NestedCssProperties = Nested<CssProperties>
+export type NestedCssProperties = Nested<CssProperties>;
 
-export type SystemStyleObject = Omit<Nested<SystemProperties & CssVarProperties>, 'base'>
+export type SystemStyleObject = Omit<Nested<SystemProperties & CssVarProperties>, 'base'>;
 
 export interface GlobalStyleObject {
-  [selector: string]: SystemStyleObject
+  [selector: string]: SystemStyleObject;
 }
 export interface ExtendableGlobalStyleObject {
-  [selector: string]: SystemStyleObject | undefined
-  extend?: GlobalStyleObject | undefined
+  [selector: string]: SystemStyleObject | undefined;
+  extend?: GlobalStyleObject | undefined;
 }
 
 /* -----------------------------------------------------------------------------
@@ -221,49 +221,49 @@ export interface ExtendableGlobalStyleObject {
  * -----------------------------------------------------------------------------*/
 
 type FilterStyleObject<P extends string> = {
-  [K in P]?: K extends keyof SystemStyleObject ? SystemStyleObject[K] : unknown
-}
+  [K in P]?: K extends keyof SystemStyleObject ? SystemStyleObject[K] : unknown;
+};
 
-export type CompositionStyleObject<Property extends string> = Nested<FilterStyleObject<Property> & CssVarProperties>
+export type CompositionStyleObject<Property extends string> = Nested<FilterStyleObject<Property> & CssVarProperties>;
 
 /* -----------------------------------------------------------------------------
  * Font face
  * -----------------------------------------------------------------------------*/
 
-export type GlobalFontfaceRule = Omit<AtRule.FontFaceFallback, 'src'> & Required<Pick<AtRule.FontFaceFallback, 'src'>>
+export type GlobalFontfaceRule = Omit<AtRule.FontFaceFallback, 'src'> & Required<Pick<AtRule.FontFaceFallback, 'src'>>;
 
-export type FontfaceRule = Omit<GlobalFontfaceRule, 'fontFamily'>
+export type FontfaceRule = Omit<GlobalFontfaceRule, 'fontFamily'>;
 
 export interface GlobalFontface {
-  [name: string]: FontfaceRule | FontfaceRule[]
+  [name: string]: FontfaceRule | FontfaceRule[];
 }
 
 export interface ExtendableGlobalFontface {
-  [name: string]: FontfaceRule | FontfaceRule[] | GlobalFontface | undefined
-  extend?: GlobalFontface | undefined
+  [name: string]: FontfaceRule | FontfaceRule[] | GlobalFontface | undefined;
+  extend?: GlobalFontface | undefined;
 }
 
 /* -----------------------------------------------------------------------------
  * Jsx style props
  * -----------------------------------------------------------------------------*/
 interface WithCss {
-  css?: SystemStyleObject | SystemStyleObject[]
+  css?: SystemStyleObject | SystemStyleObject[];
 }
 
-export type JsxStyleProps = SystemStyleObject & WithCss
+export type JsxStyleProps = SystemStyleObject & WithCss;
 
 export interface PatchedHTMLProps {
-  htmlWidth?: string | number
-  htmlHeight?: string | number
-  htmlTranslate?: 'yes' | 'no' | undefined
-  htmlContent?: string
+  htmlWidth?: string | number;
+  htmlHeight?: string | number;
+  htmlTranslate?: 'yes' | 'no' | undefined;
+  htmlContent?: string;
 }
 
-export type OmittedHTMLProps = 'color' | 'translate' | 'transition' | 'width' | 'height' | 'content'
+export type OmittedHTMLProps = 'color' | 'translate' | 'transition' | 'width' | 'height' | 'content';
 
-type WithHTMLProps<T> = DistributiveOmit<T, OmittedHTMLProps> & PatchedHTMLProps
+type WithHTMLProps<T> = DistributiveOmit<T, OmittedHTMLProps> & PatchedHTMLProps;
 
 export type JsxHTMLProps<T extends Record<string, any>, P extends Record<string, any> = {}> = Assign<
   WithHTMLProps<T>,
   P
->
+>;
