c1b74a83fc316ae088f1c0b935dcf8757bd1a7a1
Juan Pedro Bolívar Puente <raskolnikov@gnu.org>
2026-01-02 18:46:50 +0100

    Add basic Nix flake development environment

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          pkgs.nodePackages.pnpm
+        ];
+      };
+    }
+  );
+}
