15b303ae1a126f72c94cb07b44aee23658354dea
trim21 <i@trim21.me>
2026-02-22 15:21:36 +0800

    refactor(ui): list ApplicationView modifiers explicitly

--- client/src/javascript/components/layout/ApplicationView.tsx
+++ client/src/javascript/components/layout/ApplicationView.tsx
@@ -6,6 +6,7 @@ import UIStore from '@client/stores/UIStore';
 
 export enum ApplicationViewModifier {
   AuthForm = 'auth-form',
+  SidebarAlternativeState = 'sidebar-alternative-state',
 }
 
 interface ApplicationViewProps {
@@ -14,10 +15,19 @@ interface ApplicationViewProps {
 }
 
 const ApplicationView: FC<ApplicationViewProps> = observer(({children, modifier}: ApplicationViewProps) => {
+  const activeModifiers: Array<ApplicationViewModifier> = [];
+
+  if (modifier != null) {
+    activeModifiers.push(modifier);
+  }
+
+  if (UIStore.isSidebarAlternativeState) {
+    activeModifiers.push(ApplicationViewModifier.SidebarAlternativeState);
+  }
+
   const classes = classnames(
     'application__view',
-    modifier != null && `application__view--${modifier}`,
-    UIStore.isSidebarAlternativeState && 'application__view--sidebar-alternative-state',
+    ...activeModifiers.map((activeModifier) => `application__view--${activeModifier}`),
   );
 
   return <div className={classes}>{children}</div>;
