jesec/flood

commit 0f87dc2cc52ac94f6244a09312ed40895e1740de

feat: add warning status level for tracker messages (#1108)

server/services/qBittorrent/clientGatewayService.ts
@@ -49,7 +49,9 @@ class QBittorrentClientGatewayService extends ClientGatewayService {
   private clientRequestManager = new ClientRequestManager(this.user.client as QBittorrentConnectionSettings);
   private cachedProperties: Record<
     string,
-    Pick<TorrentProperties, 'comment' | 'dateCreated' | 'isPrivate' | 'trackerURIs'>
+    Pick<TorrentProperties, 'comment' | 'dateCreated' | 'isPrivate' | 'trackerURIs'> & {
+      trackerMessage: string;
+    }
   > = {};
 
   async addTorrentsByFile({
@@ -363,10 +365,12 @@ class QBittorrentClientGatewayService extends ClientGatewayService {
             const trackers = await this.clientRequestManager.getTorrentTrackers(hash).catch(() => undefined);
 
             if (properties != null && trackers != null && Array.isArray(trackers)) {
+              const firstTrackerMsg = trackers.find((t) => t.msg.length > 0)?.msg ?? '';
               this.cachedProperties[hash] = {
                 comment: properties?.comment,
                 dateCreated: properties?.creation_date,
                 isPrivate: trackers[0]?.msg.includes('is private'),
+                trackerMessage: firstTrackerMsg,
                 trackerURIs: getDomainsFromURLs(
                   trackers
                     .map((tracker) => tracker.url)
@@ -385,6 +389,7 @@ class QBittorrentClientGatewayService extends ClientGatewayService {
                 comment = '',
                 dateCreated = 0,
                 isPrivate = false,
+                trackerMessage = '',
                 trackerURIs = [],
               } = this.cachedProperties[info.hash] || {};
 
@@ -406,7 +411,7 @@ class QBittorrentClientGatewayService extends ClientGatewayService {
                 isPrivate,
                 isInitialSeeding: info.super_seeding,
                 isSequential: info.seq_dl,
-                message: '', // in tracker method
+                message: trackerMessage,
                 name: info.name,
                 peersConnected: info.num_leechs,
                 peersTotal: info.num_incomplete,
@@ -416,7 +421,7 @@ class QBittorrentClientGatewayService extends ClientGatewayService {
                 seedsConnected: info.num_seeds,
                 seedsTotal: info.num_complete,
                 sizeBytes: info.size,
-                status: getTorrentStatusFromState(info.state),
+                status: getTorrentStatusFromState(info.state, trackerMessage),
                 tags: info.tags === '' ? [] : info.tags.split(',').map((tag) => tag.trim()),
                 trackerURIs,
                 upRate: info.upspeed,

client/src/javascript/components/general/TorrentStatusIcon.tsx
@@ -1,6 +1,6 @@
 import {FC, memo} from 'react';
 
-import {Error, Spinner, Start, Stop} from '@client/ui/icons';
+import {CircleExclamation, Error, Spinner, Start, Stop} from '@client/ui/icons';
 
 import type {TorrentStatus} from '@shared/constants/torrentStatusMap';
 
@@ -10,6 +10,8 @@ interface TorrentStatusIconProps {
 
 const TorrentStatusIcon: FC<TorrentStatusIconProps> = memo(({status}: TorrentStatusIconProps) => {
   switch (status) {
+    case 'warning':
+      return <CircleExclamation />;
     case 'error':
       return <Error />;
     case 'checking':

client/src/javascript/components/sidebar/StatusFilters.tsx
@@ -2,7 +2,18 @@ import {FC, useState} from 'react';
 import {observer} from 'mobx-react-lite';
 import {useLingui} from '@lingui/react';
 
-import {Active, All, Completed, DownloadSmall, Error, Inactive, Stop, Spinner, UploadSmall} from '@client/ui/icons';
+import {
+  Active,
+  All,
+  CircleExclamation,
+  Completed,
+  DownloadSmall,
+  Error,
+  Inactive,
+  Stop,
+  Spinner,
+  UploadSmall,
+} from '@client/ui/icons';
 import TorrentFilterStore from '@client/stores/TorrentFilterStore';
 
 import type {TorrentStatus} from '@shared/constants/torrentStatusMap';
@@ -57,6 +68,11 @@ const StatusFilters: FC = observer(() => {
       label: i18n._('filter.status.inactive'),
       slug: 'inactive',
       icon: <Inactive />,
+    },
+    {
+      label: i18n._('filter.status.warning'),
+      slug: 'warning',
+      icon: <CircleExclamation />,
     },
     {
       label: i18n._('filter.status.error'),

client/src/javascript/util/torrentStatus.ts
@@ -8,6 +8,7 @@ export const torrentStatusClasses = (
 ): string =>
   classnames(classes, {
     'torrent--has-error': status.includes('error'),
+    'torrent--has-warning': status.includes('warning'),
     'torrent--is-stopped': status.includes('stopped'),
     'torrent--is-downloading': status.includes('downloading'),
     'torrent--is-downloading--actively': downRate > 0,
@@ -21,7 +22,7 @@ export const torrentStatusClasses = (
 export const torrentStatusEffective = (status: TorrentProperties['status']): TorrentProperties['status'][number] => {
   let result: TorrentProperties['status'][number] = 'stopped';
 
-  ['error', 'checking', 'stopped', 'downloading', 'seeding'].some((state) => {
+  ['warning', 'error', 'checking', 'stopped', 'downloading', 'seeding'].some((state) => {
     if (status.includes(state as TorrentProperties['status'][number])) {
       result = state as TorrentProperties['status'][number];
       return true;

client/src/sass/components/_progress-bar.scss
@@ -43,6 +43,10 @@ $progress-bar--height: 3px;
         @include themes.theme('color', 'progress-bar--fill--error');
       }
 
+      .torrent--has-warning & {
+        @include themes.theme('color', 'progress-bar--fill--warning');
+      }
+
       .torrent--is-checking & {
         @include themes.theme('color', 'progress-bar--fill--checking');
       }
@@ -57,6 +61,10 @@ $progress-bar--height: 3px;
 
       .torrent--has-error.torrent--is-stopped & {
         @include themes.theme('color', 'progress-bar--fill--error--stopped');
+      }
+
+      .torrent--has-warning.torrent--is-stopped & {
+        @include themes.theme('color', 'progress-bar--fill--warning--stopped');
       }
     }
   }
@@ -79,6 +87,10 @@ $progress-bar--height: 3px;
       @include themes.theme('background', 'progress-bar--fill--error');
     }
 
+    .torrent--has-warning & {
+      @include themes.theme('background', 'progress-bar--fill--warning');
+    }
+
     .torrent--is-checking & {
       @include themes.theme('background', 'progress-bar--fill--checking');
     }
@@ -95,6 +107,10 @@ $progress-bar--height: 3px;
       @include themes.theme('background', 'progress-bar--fill--error--stopped');
     }
 
+    .torrent--has-warning.torrent--is-stopped & {
+      @include themes.theme('background', 'progress-bar--fill--warning--stopped');
+    }
+
     &__wrapper {
       @include themes.theme('background', 'progress-bar--track--background');
       flex: 1 1 auto;
@@ -109,12 +125,20 @@ $progress-bar--height: 3px;
         @include themes.theme('background', 'progress-bar--track--background--error');
       }
 
+      .torrent--has-warning & {
+        @include themes.theme('background', 'progress-bar--track--background--warning');
+      }
+
       .torrent--is-selected & {
         @include themes.theme('background', 'progress-bar--track--background--selected');
       }
 
       .torrent--has-error.torrent--is-stopped & {
         @include themes.theme('background', 'progress-bar--track--background--error--stopped');
+      }
+
+      .torrent--has-warning.torrent--is-stopped & {
+        @include themes.theme('background', 'progress-bar--track--background--warning--stopped');
       }
 
       .torrent--is-selected.torrent--is-stopped & {

client/src/sass/components/_torrent.scss
@@ -142,6 +142,31 @@ $more-info--border: floating-action.$textbox-repeater--button--border;
     }
   }
 
+  &--has-warning {
+    &.torrent {
+      &--is-stopped {
+        .torrent {
+          &__detail {
+            &--name {
+              @include themes.theme('color', 'torrent--primary--foreground--warning--stopped');
+            }
+          }
+        }
+      }
+    }
+
+    .torrent {
+      &__detail {
+        &--name {
+          @include themes.theme('color', 'torrent--primary--foreground--warning');
+        }
+      }
+    }
+
+    &:after {
+      @include themes.theme('background', 'torrent--background--warning');
+    }
+  }
   &--is-downloading {
     &--actively {
       .torrent {
@@ -227,6 +252,35 @@ $more-info--border: floating-action.$textbox-repeater--button--border;
         }
       }
 
+      &--has-warning {
+        .torrent {
+          &__detail {
+            &--name {
+              @include themes.theme('color', 'torrent--primary--foreground--selected--warning');
+            }
+
+            &--tags {
+              .tag {
+                @include themes.theme('background', 'torrent--tags--background--selected--has-warning');
+                @include themes.theme('color', 'torrent--tags--foreground--has-warning');
+              }
+            }
+
+            &--downRate,
+            &--upRate {
+              @include themes.theme('color', 'torrent--speed--foreground--selected--has-warning');
+            }
+          }
+        }
+
+        &,
+        &:hover {
+          &:after {
+            @include themes.theme('background', 'torrent--background--warning');
+          }
+        }
+      }
+
       &--is-stopped {
         &.torrent {
           &--has-error {
@@ -234,6 +288,16 @@ $more-info--border: floating-action.$textbox-repeater--button--border;
               &__detail {
                 &--name {
                   @include themes.theme('color', 'torrent--primary--foreground--error--stopped--selected');
+                }
+              }
+            }
+          }
+
+          &--has-warning {
+            .torrent {
+              &__detail {
+                &--name {
+                  @include themes.theme('color', 'torrent--primary--foreground--warning--stopped--selected');
                 }
               }
             }
@@ -507,6 +571,13 @@ $more-info--border: floating-action.$textbox-repeater--button--border;
             &,
             & + .torrent {
               @include themes.theme('border-color', 'torrent--border--selected--error');
+            }
+          }
+
+          &--has-warning {
+            &,
+            & + .torrent {
+              @include themes.theme('border-color', 'torrent--border--selected--warning');
             }
           }
 

client/src/sass/tools/_themes.scss
@@ -52,10 +52,14 @@ $themes: (
     torrent--primary--foreground--error: colors.$red,
     torrent--primary--foreground--error--stopped: rgba(colors.$red, 0.6),
     torrent--primary--foreground--error--stopped--selected: rgba(colors.$white, 0.6),
+    torrent--primary--foreground--warning: #d97706,
+    torrent--primary--foreground--warning--stopped: rgba(#d97706, 0.6),
+    torrent--primary--foreground--warning--stopped--selected: rgba(colors.$white, 0.6),
     torrent--primary--foreground--stopped: rgba(colors.$grey, 0.8),
     torrent--primary--foreground--selected: colors.$white,
     torrent--primary--foreground--selected--stopped: rgba(colors.$white, 0.6),
     torrent--primary--foreground--selected--error: colors.$white,
+    torrent--primary--foreground--selected--warning: colors.$white,
     torrent--secondary--foreground: #8899a8,
     torrent--secondary--foreground--stopped: #c3ccd3,
     torrent--secondary--foreground--selected: colors.$white,
@@ -67,17 +71,22 @@ $themes: (
     torrent--background--hover: #f6f8fa,
     torrent--background--selected: colors.$blue,
     torrent--background--error: colors.$red,
+    torrent--background--warning: #f59e0b,
     torrent--border: 1px solid #e9eef2,
     torrent--border--selected: color.adjust(colors.$blue, $lightness: -3%),
     torrent--border--selected--error: color.adjust(colors.$red, $lightness: -3%),
+    torrent--border--selected--warning: color.adjust(#f59e0b, $lightness: -3%),
     torrent--speed--foreground--selected--has-error: colors.$white,
+    torrent--speed--foreground--selected--has-warning: colors.$white,
     torrent--speed--foreground--selected: colors.$white,
     torrent--tags--background: #dce2e6,
     torrent--tags--foreground: #6f7d87,
     torrent--tags--background--selected: #4ea3ea,
     torrent--tags--background--selected--has-error: #ed7994,
+    torrent--tags--background--selected--has-warning: #fbbf24,
     torrent--tags--foreground--selected: colors.$white,
     torrent--tags--foreground--has-error: #fbe8ec,
+    torrent--tags--foreground--has-warning: #fef3c7,
     more-info--background: colors.$white,
     more-info--background--hover: colors.$white,
     more-info--box-shadow: (
@@ -96,6 +105,8 @@ $themes: (
     progress-bar--fill--completed: colors.$blue,
     progress-bar--fill--error: #e95779,
     progress-bar--fill--error--stopped: #f2acbc,
+    progress-bar--fill--warning: #f59e0b,
+    progress-bar--fill--warning--stopped: #fcd34d,
     progress-bar--fill--selected: #fff,
     progress-bar--fill--selected--stopped: #5daaeb,
     progress-bar--fill--stopped: #e7ebee,
@@ -103,6 +114,8 @@ $themes: (
     progress-bar--track--background--completed: rgba(colors.$blue, 0.15),
     progress-bar--track--background--error: rgba(#e95779, 0.15),
     progress-bar--track--background--error--stopped: rgba(#f2acbc, 0.15),
+    progress-bar--track--background--warning: rgba(#f59e0b, 0.15),
+    progress-bar--track--background--warning--stopped: rgba(#fcd34d, 0.15),
     progress-bar--track--background--selected: rgba(#fff, 0.15),
     progress-bar--track--background--selected--stopped: rgba(#fff, 0.15),
     progress-bar--track--background--stopped: rgba(#e7ebee, 0.35),
@@ -150,10 +163,14 @@ $themes: (
     torrent--primary--foreground--error: colors.$red,
     torrent--primary--foreground--error--stopped: rgba(colors.$red, 0.6),
     torrent--primary--foreground--error--stopped--selected: rgba(colors.$white, 0.6),
+    torrent--primary--foreground--warning: #d97706,
+    torrent--primary--foreground--warning--stopped: rgba(#d97706, 0.6),
+    torrent--primary--foreground--warning--stopped--selected: rgba(colors.$white, 0.6),
     torrent--primary--foreground--stopped: rgba(colors.$grey, 0.8),
     torrent--primary--foreground--selected: colors.$white,
     torrent--primary--foreground--selected--stopped: rgba(colors.$white, 0.6),
     torrent--primary--foreground--selected--error: colors.$white,
+    torrent--primary--foreground--selected--warning: colors.$white,
     torrent--secondary--foreground: colors-dark.$md-text-secondary-dark,
     torrent--secondary--foreground--stopped: colors-dark.$md-text-disabled-dark,
     torrent--secondary--foreground--selected: colors.$white,
@@ -165,17 +182,22 @@ $themes: (
     torrent--background--hover: #f6f8fa,
     torrent--background--selected: colors.$blue,
     torrent--background--error: colors.$red,
+    torrent--background--warning: #f59e0b,
     torrent--border: 1px solid colors-dark.$md-text-dividers-dark,
     torrent--border--selected: color.adjust(colors.$blue, $lightness: -3%),
     torrent--border--selected--error: color.adjust(colors.$red, $lightness: -3%),
+    torrent--border--selected--warning: color.adjust(#f59e0b, $lightness: -3%),
     torrent--speed--foreground--selected--has-error: colors.$white,
+    torrent--speed--foreground--selected--has-warning: colors.$white,
     torrent--speed--foreground--selected: colors.$white,
     torrent--tags--background: colors-dark.$md-surface-dark-e06,
     torrent--tags--foreground: colors-dark.$md-text-secondary-dark,
     torrent--tags--background--selected: #4ea3ea,
     torrent--tags--background--selected--has-error: #ed7994,
+    torrent--tags--background--selected--has-warning: #fbbf24,
     torrent--tags--foreground--selected: colors.$white,
     torrent--tags--foreground--has-error: #fbe8ec,
+    torrent--tags--foreground--has-warning: #fef3c7,
     more-info--background: colors.$white,
     more-info--background--hover: colors.$white,
     more-info--box-shadow: (
@@ -194,6 +216,8 @@ $themes: (
     progress-bar--fill--completed: colors.$blue,
     progress-bar--fill--error: #e95779,
     progress-bar--fill--error--stopped: #f2acbc,
+    progress-bar--fill--warning: #f59e0b,
+    progress-bar--fill--warning--stopped: #fcd34d,
     progress-bar--fill--selected: #fff,
     progress-bar--fill--selected--stopped: colors-dark.$md-text-secondary-dark,
     progress-bar--fill--stopped: colors-dark.$md-text-disabled-dark,
@@ -201,6 +225,8 @@ $themes: (
     progress-bar--track--background--completed: rgba(colors.$blue, 0.15),
     progress-bar--track--background--error: rgba(#e95779, 0.15),
     progress-bar--track--background--error--stopped: rgba(#f2acbc, 0.15),
+    progress-bar--track--background--warning: rgba(#f59e0b, 0.15),
+    progress-bar--track--background--warning--stopped: rgba(#fcd34d, 0.15),
     progress-bar--track--background--selected: rgba(#fff, 0.15),
     progress-bar--track--background--selected--stopped: colors-dark.$md-text-dividers-dark,
     progress-bar--track--background--stopped: colors-dark.$md-text-dividers-dark,

server/services/Transmission/util/torrentPropertiesUtil.ts
@@ -42,7 +42,11 @@ const getTorrentStatus = (
   }
 
   if (error !== TransmissionTorrentError.TR_STAT_OK) {
-    statuses.push('error');
+    if (error === TransmissionTorrentError.TR_STAT_TRACKER_WARNING) {
+      statuses.push('warning');
+    } else {
+      statuses.push('error');
+    }
   }
 
   if (haveValid === totalSize) {

commit b50096ac6a12421929d9cf4abfc2b692c9d99af4

fix: filter qBittorrent trackers by tier>=0 for message extraction
DHT/PeX/LSD trackers (tier < 0) send informational messages like
"This torrent is private" that were incorrectly triggering the
warning status introduced in #1108. Only consider real trackers
(tier >= 0) when extracting tracker messages and building URIs.

Fixes #1118

server/services/qBittorrent/clientGatewayService.ts
@@ -368,17 +368,14 @@ class QBittorrentClientGatewayService extends ClientGatewayService {
             const trackers = await this.clientRequestManager.getTorrentTrackers(hash).catch(() => undefined);
 
             if (properties != null && trackers != null && Array.isArray(trackers)) {
-              const firstTrackerMsg = trackers.find((t) => t.msg.length > 0)?.msg ?? '';
+              const realTrackers = trackers.filter((t) => t.tier >= 0);
+              const firstTrackerMsg = realTrackers.find((t) => t.msg.length > 0)?.msg ?? '';
               this.cachedProperties[hash] = {
                 comment: properties?.comment,
                 dateCreated: properties?.creation_date,
                 isPrivate: trackers[0]?.msg.includes('is private'),
                 trackerMessage: firstTrackerMsg,
-                trackerURIs: getDomainsFromURLs(
-                  trackers
-                    .map((tracker) => tracker.url)
-                    .filter((url) => getTorrentTrackerTypeFromURL(url) !== TorrentTrackerType.DHT),
-                ),
+                trackerURIs: getDomainsFromURLs(realTrackers.map((tracker) => tracker.url)),
               };
             }
           }