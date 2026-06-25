fd97b02d21250df6745e5dd82f2ee832be3e2f40
euri10 <benoit.barthelet@gmail.com>
2026-06-25 07:17:18 +0200

    fix: dont ignore dotfiles anymore

--- server/routes/api/torrents.ts
+++ server/routes/api/torrents.ts
@@ -832,6 +832,7 @@ const torrentsRoutes = async (fastify: FastifyInstance) => {
         const result = await send(request.raw, encodeURI(file), {
           acceptRanges: true,
           lastModified: true,
+          dotFiles: 'allow',
         });
 
         const statusCode = result.statusCode;
