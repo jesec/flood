ea8b43706f10b22f4d5f3f3244fe65e8195b82f7
Trim21 <trim21.me@gmail.com>
2026-07-05 18:31:16 +0800

    feat(neptune): use official SDK and support get_transfer_config (#1171)

---
+++ server/services/Neptune/sdk/index.ts
@@ -0,0 +1,32 @@
+export type {NeptuneClientOptions, NeptuneMethod, NeptuneMethodMap} from './client.ts';
+export {NeptuneClient} from './client.ts';
+export {NeptuneHTTPError, NeptuneRPCError} from './errors.ts';
+export type {
+  AddTorrentParams,
+  AddTorrentResult,
+  AddTrackerParams,
+  DelCustomParams,
+  GlobalSpeedLimitParams,
+  InfoHashParams,
+  ListTorrentParams,
+  MoveTorrentParams,
+  Peer,
+  RemoveTorrentParams,
+  RemoveTrackerParams,
+  ReplaceTrackersParams,
+  SetCustomParams,
+  SetFilePriorityParams,
+  SpeedLimitParams,
+  TagsParams,
+  Torrent,
+  TorrentFile,
+  TorrentFiles,
+  TorrentInfo,
+  TorrentList,
+  TorrentPeers,
+  TorrentState,
+  TorrentTrackers,
+  Tracker,
+  TransferSummary,
+  UpdateCustomParams,
+} from './types.ts';
