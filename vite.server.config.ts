8410babeda8ed3a729d93282e10c33088aefbd7a
Trim21 <trim21.me@gmail.com>
2026-07-26 02:38:27 +0800

    build: replace vite-tsconfig-paths plugin with native Vite support (#1334)

--- vite.server.config.ts
+++ vite.server.config.ts
@@ -1,6 +1,7 @@
 import {defineConfig} from 'vite';
-import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths()],
+  resolve: {
+    tsconfigPaths: true,
+  },
 });
